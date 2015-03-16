909e6c39094d17044b0faf2e626dfc2271e80d92
zhangyaoyao <zyy910203@126.com>
2015-03-16 13:46:32 +0800

    add some web

---
+++ enterpr.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+	<?php require ('head.html');?>
+	<body class="pattern8"><!-- Available classes for body: boxed , pattern1...pattern10 . Background Image - example add: data-background="assets/images/boxed_background/1.jpg"  -->
+
+		<?php require('header.html');?>
+
+		<div id="wrapper">
+
+			<div class="fullwidthbanner-container roundedcorners">
+				<div class="fullwidthbanner">
+					<ul>	
+						<li data-transition="fade" data-slotamount="7" data-masterspeed="300" >
+							<img src="assets/images/dummy.png" alt="大镭" data-lazyload="assets/images/dalei/en.jpg" data-fullwidthcentering="on">
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<!-- ONE NATION -->
+			<section class="container">
+				<h2><strong>企业文化</strong></h2>
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-10">
+					
+					<!-- CALLOUT -->
+				    <div class="bs-callout text-center">
+					    <h3><big>使命</big> - <strong>瞄准国际尖端技术，集合各方技术优势，不断推出先进的加速器产品，<br/>形成大镭核心竞争力，为核技术应用产业发展作贡献。</strong> </h3>
+				    </div>
+					<!-- /CALLOUT -->
+
+					<!-- CALLOUT -->
+					<div class="bs-callout text-center styleBackground ">
+						<h3><big>宗旨</big> - <strong>让电子直线加速器成为简单普及的工业设备<br/>让大镭成为加速器设备制造国内领先</strong>  </h3>
+					</div>
+					<!-- /CALLOUT -->
+
+					<!-- CALLOUT -->
+					<div class="bs-callout text-center ">
+						<h3><big>企业精神</big> - <strong>注重品质,追求完美;自信自立，自强不息</strong>  </h3>
+					</div>
+					<!-- /CALLOUT -->
+
+					<!-- CALLOUT -->
+					<div class="bs-callout text-center styleBackground">
+						<h3><big>价值观</big> - <strong>诚实守信、勤奋严谨、协作创新</strong>  </h3>
+					</div>
+				</div>
+			</section>
+			<!-- /ONE NATION -->
+
+
+        </div>
+
+
+		
+
+
+        <?php require('footer.html');?>
+
+
+		<!-- JAVASCRIPT FILES -->
+		<script type="text/javascript" src="assets/plugins/jquery-2.0.3.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="assets/plugins/jquery.cookie.js"></script>
+		<script type="text/javascript" src="assets/plugins/jquery.appear.js"></script>
+		<script type="text/javascript" src="assets/plugins/jquery.isotope.js"></script>
+		<script type="text/javascript" src="assets/plugins/masonry.js"></script>
+
+		<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/stellar/jquery.stellar.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/knob/js/jquery.knob.js"></script>
+		<script type="text/javascript" src="assets/plugins/jquery.backstretch.min.js"></script>
+		
+		<!-- REVOLUTION SLIDER -->
+		<script type="text/javascript" src="assets/plugins/revolution-slider/js/jquery.themepunch.plugins.min.js"></script>
+		<script type="text/javascript" src="assets/plugins/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+		<script type="text/javascript" src="assets/js/slider_revolution.js"></script>
+
+		<script type="text/javascript" src="assets/js/scripts.js"></script>
+
+		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information. -->
+		<!--<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-XXXXX-X', 'domainname.com');
+			ga('send', 'pageview');
+		</script>
+		-->
+
+	</body>
+</html>
