9d2c8e67ab376b2e15f014a20962c3fb0b8c12e1
lirenyue <1064272312@qq.com>
2015-03-30 16:58:15 +0800

    changed medical page

--- application-medical.php
+++ application-medical.php
@@ -54,33 +54,36 @@
 
 						    <h4><strong>中成药</strong></h4>
 
-						    <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;卫生部标准规定:</strong></h5>
+						    <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;卫生部标准规定:</strong></h5>
                             <ul>
                               <li><P style="color:#bbb">60Co辐照仅是中药灭菌的辅助手段,各级卫生行政部门要按照有关法规监督“标准”的执行,防治滥用辐照灭菌。</P></li>
                               <li><P style="color:#bbb">有关生产企业应按照《药品生产质量管理规范》(GMP)要求,重视对所用药材使用前进行拣选、整理、洗涤等前处理加工,生产过程控制在不同洁净级别的厂房内,尽量把微生物的含量控制在一定范围内,如果微生物含量超标,再辅以适宜的灭菌方法,保证药品卫生质量的。</P></li>
                               <li><P style="color:#bbb">在应用中辐照剂量的选择非常重要,应以最小的剂量达到最大的灭菌效果,尽量减少重复辐照,保证药物质量。</P></li>
                             </ul>
 
-							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌对药品质量影响</strong></h5>
+							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌对药品质量影响</strong></h5>
                             <ul>
-                              <li>对化学成分的影响<P style="color:#bbb">现今已有许多国家如德国、前苏联、印度等10多个国家对百余种药品进行了研究,对化学药品研究较多,研究结果表明:药品受照射,在固态时多数是稳定的,少数分解不严重,其分解程度与辐照剂量有关,而剂量又与污染的菌种、数量等相关。水溶液几乎均受到严重破坏。对一部分药品可以采用先辐照固态灭菌,然后再配成溶液的做法。大部分经一定剂量射线辐照的中药材,其化学成分在辐照前后无明显变化</P></li>
-                              <li>对中药材及制剂外观的影响<P style="color:#bbb">辐照对大多数中药材及中成药的外观没有影响</P></li>
-                              <li>对药品生理活性的影响<P style="color:#bbb">大多数中药及制剂经低于10kGy的射线辐照,其生物活性不受影响</P></li>
-                              <li>对药品安全性的影响<P style="color:#bbb">目前用于药品辐射的射线不超过允许的最大剂量,不会使药品产生感生放射性,不存在放射性污染</P></li>
+                              <li>对化学成分的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;现今已有许多国家如德国、前苏联、印度等10多个国家对百余种药品进行了研究,对化学药品研究较多,研究结果表明:药品受照射,在固态时多数是稳定的,少数分解不严重,其分解程度与辐照剂量有关,而剂量又与污染的菌种、数量等相关。水溶液几乎均受到严重破坏。对一部分药品可以采用先辐照固态灭菌,然后再配成溶液的做法。大部分经一定剂量射线辐照的中药材,其化学成分在辐照前后无明显变化</P></li>
+                              <li>对中药材及制剂外观的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照对大多数中药材及中成药的外观没有影响</P></li>
+                              <li>对药品生理活性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大多数中药及制剂经低于10kGy的射线辐照,其生物活性不受影响</P></li>
+                              <li>对药品安全性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前用于药品辐射的射线不超过允许的最大剂量,不会使药品产生感生放射性,不存在放射性污染</P></li>
                             </ul>
 
-							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;灭菌效果与辐照剂量</strong></h5>
+							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;灭菌效果与辐照剂量</strong></h5>
                             <ul>
                               <li><P style="color:#bbb">辐照剂量越大,灭菌效果越好。但辐照剂量过大,对药品中某些有效成分可能会有一定的影响,因此在具体的试验中应确定最佳辐照剂量,并按最佳剂量进行辐照</P></li>
                               <li><P style="color:#bbb">美国药典规定高剂量有效灭菌为 2.5 kGy,中剂量为 1 kGy,低剂量为 0.2～0.4 kGy</P></li>
                               <li><P style="color:#bbb">我国卫生部的标准规定辐照中药最高耐受剂量为:散剂、片剂 3 kGy；丸剂 5 kGy；中药原料粉 6 kGy</P></li>
                             </ul>
-          
+ 							</br>
+
                             <h4><strong>医疗用品辐射灭菌</strong></h4>
-                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;工作原理</strong></h5>
+                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;工作原理</strong></h5>
                             <ul><li><P style="color:#bbb">利用电子直线加速器产生的高能电子射线的化学生物效应，以达到杀灭病原微生物的目的。</P></li></ul>
 
-                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;医疗用品辐射灭菌是一项新技术，比蒸汽或环氧乙烷（Eto)灭菌法有许多优点：</strong></h5>
+							</br>
+
+                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;医疗用品辐射灭菌是一项新技术，比蒸汽或环氧乙烷（Eto)灭菌法有许多优点：</strong></h5>
 							<ul>
                               <li><P style="color:#bbb">辐射灭菌可在室温下进行，适用于热敏感的塑料制品、生物体和药物；</P></li>
                               <li><P style="color:#bbb">辐射穿透力强，杀菌均匀彻底，大量采用经辐射灭菌的一次性使用的医疗用品，可避免交叉感染降低发病率；</P></li>
