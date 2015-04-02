d87a926c93f86dd29946e203b094b3d60334ff21
renyue li <1064272312@qq.com>
2015-04-02 16:51:51 +0800

    solved mistakes of application picture

--- irradi-applications.php
+++ irradi-applications.php
@@ -51,23 +51,23 @@
                             <div class="divider half-margins"><!-- divider -->
 							    <i class="fa fa-star"></i>
 						    </div>
-					    	
-						    <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电子直线加速器的使用已经深入到国民经济的各个领域,目前使用最多的领域是医院的癌症治疗,其次是工业应用领域的辐照加工,辐照改性和工业探伤,另外射线的无损检查,如海关集装箱检查等也可以归到工业应用领域,直线加速器的应用在以上两个领域已得到了普遍认同和普及。</P>
-               
-                <div class="row">
-                    <div class="col-md-1">
-                    </div>
-                    <div class="col-md-10">
-                        <figure>
-                            <img class="img-responsive" src="assets/images/dalei/application.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备应用"/>
-                        </figure>
-                    </div>
-                    <div class="col-md-1">
-                    </div>
-                <div>
-                <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照应用简表</strong></h4>
+			 <div class="row">
+                           <div class="col-md-1">
+                           </div>
+                           <div class="col-md-10">
+                                <img class="img-responsive" src="assets/images/dalei/application.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备应用"/>
+                           </div>
+                           <div class="col-md-1">
+                           </div>
+                        </div>
 
-						    <div class="col-md-12"><br/>
+                        </br>
+		    	
+						    <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电子直线加速器的使用已经深入到国民经济的各个领域，目前使用最多的领域是医院的癌症治疗，其次是工业应用领域的辐照加工，辐照改性和工业探伤，另外射线的无损检查，如海关集装箱检查等也可以归到工业应用领域，直线加速器的应用在以上两个领域已得到了普遍认同和普及。上图为浙江大镭核技术应用设备有限公司舟山辐照中心，图片来自《舟商杂志》。</P>
+               
+
+						    <div class="col-md-12">
+                <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照应用简表</strong></h4>
 
 				                <table class="table" style="background-color:rgba(0,0,0,0.1); color:rgba(255,255,255,1);">
 				                   	<tr>
