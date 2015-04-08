c894bd95d5858a3ec6b94298cf6a238181aad138
lirenyue <1064272312@qq.com>
2015-04-08 19:27:55 +0800

    add a comparison table about our production and co60 device in application-technology

--- application-technology.php
+++ application-technology.php
@@ -55,17 +55,78 @@
 						    <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;原理：将电子加速器（0.2MeV~10MeV）产生的电子射线（β射线）或放射性同位素（Cs-137或Co-60）产生的γ射线的能量转移给被辐照物质，电离辐射作用到被辐照的物质上，产生电离和激发，释放出轨道电子，形成自由基，通过控制辐射条件，而使被辐照物质的物理性能和化学组成发生变化并能使其成为人们所需要的一种新的物质，或使生物体（微生物等）受到不可恢复的损失和破坏，达到人们所需要的目标。</p>
 						    <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前使用的射线源装置主要有：</p>
 				     	    <ul>
-				     	    	<li><P style="color:#bbb">微波电子直线加速器</P></li>
+				     	    	<li><P style="color:#bbb">电子直线加速器</P></li>
 				     	    	<li><P style="color:#bbb">钴60辐照装置</P></li>
-							    <li><P style="color:#bbb">静电加速器（范得格拉夫电子加速器）</P></li>
 							    <li><P style="color:#bbb">高频高压加速器（地那米加速器）</P></li>
-							    <li><P style="color:#bbb">绝缘磁芯变压器</P></li>
-							    <li><P style="color:#bbb">高压倍加器</P></li>
-							    <li><P style="color:#bbb">脉冲电子加速器</P></li>
 						    </ul>	
-						    <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;钴60装置、高压型加速器、电子直线加速器比较：</p>
 
-						    <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射防护：</p>
+						    </br>
+
+                            <li><P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;针对电子直线加速器结构复杂、故障率高、维护难的特点，大镭投入了大量的人力物力，目前已取得了重要突破。主要成果如下：</P>
+                            <ul>
+                              <li><P style="color:#bbb"><strong>采用全新的智能化控制系统取代传统的硬件开关、按钮控制，优于市场上任意一款同类产品。</strong></P></li>
+                              <li><P style="color:#bbb">智能化的故障预测与检测系统，能及时提示设备的故障源，省去故障查找过程，极大的缩短了由故障率高而导致的停机时间。</P></li>
+                              <li><P style="color:#bbb">傻瓜式的控制流程，有效地解决客户开关机逻辑复杂的难题。</P></li>
+                              <li><P style="color:#bbb">采用远程终端，使设备不再受时间和地域限制，随时随地查看设备运行情况。</P></li>
+                              <li><P style="color:#bbb">全新开发的友好人机界面，改变工业设备界面单调、乏味的局面。</P></li>
+                              <li><P style="color:#bbb">基于远程终端开发了多样性应用，既有控制系统，更有辐照工艺管理系统，使得整个设备和工艺流程融为一体，使设备自动化程度上了一个新的台阶。</P></li>
+                            </ul>
+                          
+                        </li>
+
+                        <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电子直线加速器、钴60射线源装置比较：</p>
+
+						<div class="col-md-12"><br/>
+                        <table class="table" style="background-color:rgba(0,0,0,0.1); color:rgba(255,255,255,1);">
+                          <tr>
+                            <th width="" style="border-top:1px dashed rgba(102,102,102,0.5); "><span>比较因素</span></th>
+                            <th width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>电子直线加速器</span></th>
+                            <th width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>钴60射线源装置</span></th>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>建源成本</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>市场价1500万左右</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>23元/居里（200万居里钴源，购源成本近5000万） </span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>使用成本</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>不消耗能量，仅消耗电能以及维护成本
+</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>半衰期为5.3年，过2年必须进行新源的补充，否则将面临关机</span></td>
+                          </tr>
+                          <tr>
+                            <td width="" style="border-top:1px dashed rgba(102,102,102,0.5); "><span>后期处理</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>无</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>发达国家处理钴源费用为购买费用的6~7倍</span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>政策优势</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>国外鼓励发展加速器替代钴源辐照。美国已经禁止发展股源，我国也在提倡发展加速器辐照</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>50万以下的新建钴源不再审批，重点发展200万居里以上的辐照中心</span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>应用范围</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>能量高，应用范围广</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>能量低，应用范围受限</span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>射线利用率</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>加速器射线同一方向，利用率高达93%以上</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>钴60源呈球状发射，天然放射源，不可控，即使无货物依然在消耗，利用率低</span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>安全感</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>较高，关机后无辐射，各种电气联锁和机械联锁保证工作人员的人身安全</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>较低，天然放射源不受控，且“费源”处理出差错易引起社会恐慌</span></td>
+                          </tr>
+                          <tr>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>技术难度</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>设备结构复杂，技术难度大,故障率高</span></td>
+                            <td width=""  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>技术简单</span></td>
+                          </tr>
+                      </table> 
+                </div>    
+
                         </div>
 
 				    </div>
