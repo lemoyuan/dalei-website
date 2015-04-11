1f903304d739ce2d23699f785f08ce22cfd0270b
lirenyue <1064272312@qq.com>
2015-04-11 15:38:27 +0800

    changed layout of industry application

--- application-industry.php
+++ application-industry.php
@@ -52,36 +52,73 @@
 							    <i class="fa fa-star"></i>
 						    </div>
 
-                            <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;聚合物的辐射交联</strong></h4>
-                            <p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;聚合物的辐射交联就是在一定剂量的辐射作用下，聚合物大分子间形成化学键（即横键或交联键），导致分子量增加，并使聚合物的线状分子互相结合，把线状分子结构转变成热固性聚合物那样的网状大分子结构。这样，聚合物原有的性质就会有很大的改变。例如辐射交联电线电缆、低温阻燃柔软热缩细管等。</p>
+                            <p style="color:#bbb">辐照技术在工业领域，特别是辐射化工、工业“三废”处理、材料合成、材料改性、半导体辐照等方面都有着广泛的应用。</p>
                   
                             <br/>
 
-                            <h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;聚乙烯泡沫塑料</strong></h4>
-                			<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;聚乙烯经辐射交联后才有一定强度，能将分解气体包埋住，产品具有独立的微细气孔，表面柔滑美观、加工性能好，并具有优良的耐腐蚀性、消冲击性、绝缘和绝热性能。</p>
+							<div class="toogle toogle-accordion">
 
-                			<br/>
+							<div class="toggle active">
+								<label>聚合物的辐射交联</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label style="color:#428bca">辐射交联技术</label>就是在一定剂量的辐射作用下，聚合物大分子间形成化学键（即横键或交联键），导致分子量增加，并使聚合物的线状分子互相结合，把线状分子结构转变成热固性聚合物那样的网状大分子结构。这样，聚合物原有的性质就会有很大的改变。例如。</p>
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射交联技术应用面广，产业规模大。目前，国外辐射化工领域规模化的产业主要有电线电缆、热收缩制品、汽车轮胎、聚烯烃发泡材料、表面涂层固化、高分子PTC器件等。这几类主导型产品约占辐射化工年总产值的95%以上，创造了可观的经济效益。纵观国内电线电缆、低温阻燃柔软热缩细管等产品辐照较多，尚存在巨大的发展潜力。</p>
+								</div>
+							</div>
 
-                			<h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照去污</strong></h4>
-                			<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;污水通常是采用“活性污泥法”进行处理的。由此产生的沉积物、淤渣泥浆需要进一步处理。污泥浆本身含有很多磷、氮等优质肥料。但是另一方面，人们也担心在污泥中隐藏了各种各样的细菌。因此，先要用射线进行辐照灭菌，然后才能用作肥料。</p>
-                  
-                			<br/>
+							<div class="toggle">
+								<label>聚乙烯泡沫塑料</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;聚乙烯经辐射交联后才有一定强度，能将分解气体包埋住，产品具有独立的微细气孔，表面柔滑美观、加工性能好，并具有优良的耐腐蚀性、消冲击性、绝缘和绝热性能。</p>
+								</div>
+							</div>
 
-                			<h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照致色</strong></h4>
-                			<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射源发出的高能粒子穿过宝石晶体时,晶体内部会产生不同类型的点阵缺陷(空位、离位原子或离子),进而诱发新色心的形成,导致颜色改变。常见的有色宝石辐照处理有蓝宝石、托帕石、碧玺、长石、方柱石、锂辉石、方解石、珍珠和石英等</p>
-                
-                			<br/>
+							<div class="toggle">
+								<label>辐照去污</label>
+								<div class="toggle-content">
+									<p>电离辐射导致的<label style="color:#428bca">辐射分解及辐射合成反应</label>被用于处理工业三废，例如火力发电厂烟道气辐射脱硫脱硝，同时合成出化肥，非常有利于资源的综合利用。</p>
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;污水处理通常是采用“活性污泥法”，由此产生的沉积物、淤渣泥浆需要进一步处理。污泥浆本身含有很多磷、氮等优质肥料。但是，污泥中同时隐藏了各种各样的细菌。用辐射技术处理工业废水，能去除废水中的有机氯和重金属离子、杀灭微生物，然后将产物用作肥料。</p>
+								</div>
+							</div>
 
-                			<h4><strong>&nbsp;&nbsp;&nbsp;&nbsp;半导体器件辐照</strong></h4>
-                			<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;高能电子轰击半导体器件时,可穿透管壳或芯片进入硅体内,即能产生电离效应,又能产生位移效应。由于辐照电子与物质晶格相互作用,从而形成了空穴间隙原子对,破坏了晶格的位置,这些空位将进一步与杂质或其它空位作用而形成更复杂的缺陷,如氧空位对、磷空位对等。因而在禁带中形成了新的电子能级,不同的能级将根据其在禁带中的位置,对电子空穴俘获截面的大小和能级密度的大小均对非平衡载流子的复合有贡献, 从而引起少子寿命和载流子浓度的降低,结果使得快速二极管 trr、VF,可控硅 tq、VT等参数发生了变化。</p>
-                			<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;与传统掺金工艺相比，采用高能电子辐照半导体器件具备如下特点：
-                			<ul>
-                              <li><P style="color:#bbb">工艺可控性好,少子寿命可得到精确控制,器件参数的一致性及重复性好,成品率高,这是掺金工艺无法比拟的。特别是超快速器件采用掺金工艺是无法达到的。</P></li>
-                              <li><P style="color:#bbb">加工简便、灵活性大.由于高能电子能量高、穿透力强可直接用于封装成形的器件,这大大简化了工艺和设计,避免了掺质工艺中的沾污，提高了器件的稳定性。</P></li>
-                              <li><P style="color:#bbb">器件高温性能好,克服了掺金工艺的致命弱点。</P></li>
-                              <li><P style="color:#bbb">辐照后器件 trr-VF、 tq-VT折衷关系好。</p></li>
-                              <li><P style="color:#bbb">对掺金器件参数不合格品还可补照.同时还解决了人们最关心的辐照器件的稳定性问题。</P></li>
-                            </ul>
+							<div class="toggle">
+								<label>辐照致色</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射源发出的高能粒子穿过宝石晶体时,晶体内部会产生不同类型的点阵缺陷(空位、离位原子或离子),进而诱发新色心的形成,导致颜色改变。常见的有色宝石辐照处理有蓝宝石、托帕石、碧玺、长石、方柱石、锂辉石、方解石、珍珠和石英等</p>
+								</div>
+							</div>
+
+							<div class="toggle">
+								<label>涂层固化</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射固化与紫外固化正成为互补性技术，应用范围广，构成表面涂层加工的重要产业。辐射固化是利用低能电子束（EB）或紫外光（UV）照射液体低聚物涂层（膜）使其快速固化的技术。EB固化具有室温固化、节能高效、零污染、涂层不畸变等优点，主要加工大、厚、重的物件，与EB固化相比UV固化有投资少，成本低等竞争优势，适合加工小、薄、轻的物件。两种技术路线构成互补，目前，在日本、北美、欧洲已经形成较大的产业规模。</p>
+								</div>
+							</div>
+
+							<div class="toggle">
+								<label>半导体器件辐照</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;高能电子轰击半导体器件时,可穿透管壳或芯片进入硅体内,即能产生电离效应,又能产生位移效应。由于辐照电子与物质晶格相互作用,从而形成了空穴间隙原子对,破坏了晶格的位置,这些空位将进一步与杂质或其它空位作用而形成更复杂的缺陷,如氧空位对、磷空位对等。因而在禁带中形成了新的电子能级,不同的能级将根据其在禁带中的位置,对电子空穴俘获截面的大小和能级密度的大小均对非平衡载流子的复合有贡献, 从而引起少子寿命和载流子浓度的降低,结果使得快速二极管 trr、VF,可控硅 tq、VT等参数发生了变化。</p>
+                					<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;与传统掺金工艺相比，采用高能电子辐照半导体器件具备如下特点：
+                					<ul>
+                              			<li><P style="color:#bbb">工艺可控性好,少子寿命可得到精确控制,器件参数的一致性及重复性好,成品率高,这是掺金工艺无法比拟的。特别是超快速器件采用掺金工艺是无法达到的。</P></li>
+                              			<li><P style="color:#bbb">加工简便、灵活性大.由于高能电子能量高、穿透力强可直接用于封装成形的器件,这大大简化了工艺和设计,避免了掺质工艺中的沾污，提高了器件的稳定性。</P></li>
+                              			<li><P style="color:#bbb">器件高温性能好,克服了掺金工艺的致命弱点。</P></li>
+                             			<li><P style="color:#bbb">辐照后器件 trr-VF、 tq-VT折衷关系好。</p></li>
+                              			<li><P style="color:#bbb">对掺金器件参数不合格品还可补照.同时还解决了人们最关心的辐照器件的稳定性问题。</P></li>
+                            		</ul>
+								</div>
+							</div>
+
+							<div class="toggle">
+								<label>新材料合成与材料改性</label>
+								<div class="toggle-content">
+									<p style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐射聚合、辐射接枝技术产业规模较小，但在新材料合成与材料改性方面发挥着重要作用。水性乳胶、离子交换膜是其中代表性的产品，特别是由辐射乳液聚合获得的水性乳胶，质纯性优，没有残存易挥发有机物，更符合环保要求。用水性乳胶调制的乳胶漆，水性家具漆、地板漆等是名符其实的环保涂料。目前这类产品尚处于产业化前期，对经济社会的贡献率较低，但是，作为一种新技术，用于对新材料的研制与开发具有良好的发展前景。</p>
+								</div>
+							</div>
+
+						</div>
+                			
 				    </div>
 						
 				</div>
