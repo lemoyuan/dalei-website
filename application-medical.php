f1069e3836795e2c2ce87b61bc4c1be68fb52c59
lirenyue <1064272312@qq.com>
2015-04-14 13:59:35 +0800

    add images

--- application-medical.php
+++ application-medical.php
@@ -44,76 +44,148 @@
 
 				    </aside>
 
-                    <div class="col-md-10" style="padding:20px">
-				    	<div class="col-md-12">
+            <div class="col-md-10" style="padding:20px">
+				    	   <div class="col-md-12">
 
-						    <h3><strong>辐照技术在医疗保健产品领域的应用</strong></h3>
-                <div class="divider half-margins"><!-- divider -->
-							    <i class="fa fa-star"></i>
-						    </div>
+						          <h3><strong>辐照技术在医疗保健产品领域的应用</strong></h3>
+                      <div class="divider half-margins"><!-- divider -->
+							             <i class="fa fa-star"></i>
+						          </div>
 
-                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;什么是医疗保健产品？</strong></h5>
-                            <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;医疗保健产品是指<label>医用器械</label>、<label>医药产品（药品和生物制品）</label>及<label>体外诊断用品</label>。医用器械是指用于人体治疗、诊断等用品，设备、工具、器械、移植敷料或其他类似物品。</P>
-                            </br>
+                      <P style="color:#bbb"><label style="color:#428bca">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;医疗保健产品</label>是指<label>医用器械</label>、<label>医药产品</label>（药品和生物制品）及<label>体外诊断用品</label>。医用器械是指用于人体治疗、诊断等用品，设备、工具、器械、移植敷料或其他类似物品。</P>
 
-                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌消毒原理</strong></h5>
-                            <ul><li><P style="color:#bbb">利用电子直线加速器产生的 10Mev 以下的高能电子射线的化学生物效应，以达到杀灭病原微生物的目的。</P></li></ul>
+                      <P style="color:#bbb"><label style="color:#428bca">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌消毒原理</label>：利用电子直线加速器产生的 10Mev 以下的高能电子射线的化学生物效应，以达到杀灭病原微生物的目的。</P>
 
-							</br>
+                      <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;医疗保健产品辐射灭菌是一项新技术，比蒸汽或环氧乙烷（Eto)灭菌法有许多优点：</strong></h5>
+							        <ul>
+                          <li><P style="color:#bbb">辐射灭菌可在<label style="color:#428bca">室温</label>下进行，适用于热敏感的塑料制品、生物体和药物；</P></li>
+                          <li><P style="color:#bbb">辐射穿透力强，<label style="color:#428bca">杀菌均匀彻底</label>，大量采用经辐射灭菌的一次性使用的医疗用品，可避免交叉感染降低发病率；</P></li>
+                          <li><P style="color:#bbb">辐射灭菌<label style="color:#428bca">速度快</label>，操作简便，稳定可靠；</P></li>
+                          <li><P style="color:#bbb">辐射灭菌后的物品<label style="color:#428bca">无毒性</label>、<label style="color:#428bca">无副作用</label>、<label style="color:#428bca">无残留物</label>、安全环保。</P></li>
+                      </ul>
+                      </br>
 
-                            <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;医疗保健产品辐射灭菌是一项新技术，比蒸汽或环氧乙烷（Eto)灭菌法有许多优点：</strong></h5>
-							<ul>
-                              <li><P style="color:#bbb">辐射灭菌可在室温下进行，适用于热敏感的塑料制品、生物体和药物；</P></li>
-                              <li><P style="color:#bbb">辐射穿透力强，杀菌均匀彻底，大量采用经辐射灭菌的一次性使用的医疗用品，可避免交叉感染降低发病率；</P></li>
-                              <li><P style="color:#bbb">辐射灭菌速度快，操作简便，稳定可靠。</P></li>
-                              <li><P style="color:#bbb">辐射灭菌后的物品无毒性、无副作用、无残留物、安全环保。</P></li>
-                            </ul>
-                            </br>
+						          <h4><strong>中成药辐照灭菌</strong></h4>
+                      <div class="row text-center">
 
-						    <h4><strong>中成药辐照灭菌</strong></h4>
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/medical-1.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
 
-						    <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;卫生部标准规定:</strong></h5>
+                          </div>
+
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/medical-2.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                          </figure>
+
+                          </div>
+
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/medical-3.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
+
+                          </div>
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/medical-4.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
+
+                          </div>
+
+                      </div> 
+
+
+						          <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;卫生部标准规定:</strong></h5>
+                      <ul>
+                          <li><P style="color:#bbb">60Co辐照仅是中药灭菌的<label style="color:#428bca">辅助手段</label>,各级卫生行政部门要按照有关法规监督“标准”的执行,防止滥用辐照灭菌。</P></li>
+                          <li><P style="color:#bbb">有关生产企业应按照《药品生产质量管理规范》(GMP)要求,重视对所用药材使用前进行拣选、整理、洗涤等前处理加工,生产过程控制在不同洁净级别的厂房内,尽量把微生物的含量控制在一定范围内,如果微生物含量超标,再辅以适宜的灭菌方法,保证药品卫生质量的。</P></li>
+                          <li><P style="color:#bbb">在应用中辐照剂量的选择非常重要,应<label style="color:#428bca">以最小的剂量达到最大的灭菌效果</label>,尽量减少重复辐照,保证药物质量。</P></li>
+                      </ul>
+							        </br>
+
+							        <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌对药品质量影响</strong></h5>
+                      <ul>
+                          <li>对化学成分的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;现今已有许多国家如德国、前苏联、印度等10多个国家对百余种药品进行了研究，对化学药品研究较多，研究结果表明:</P>
                             <ul>
-                              <li><P style="color:#bbb">60Co辐照仅是中药灭菌的辅助手段,各级卫生行政部门要按照有关法规监督“标准”的执行,防治滥用辐照灭菌。</P></li>
-                              <li><P style="color:#bbb">有关生产企业应按照《药品生产质量管理规范》(GMP)要求,重视对所用药材使用前进行拣选、整理、洗涤等前处理加工,生产过程控制在不同洁净级别的厂房内,尽量把微生物的含量控制在一定范围内,如果微生物含量超标,再辅以适宜的灭菌方法,保证药品卫生质量的。</P></li>
-                              <li><P style="color:#bbb">在应用中辐照剂量的选择非常重要,应以最小的剂量达到最大的灭菌效果,尽量减少重复辐照,保证药物质量。</P></li>
+                                <li><P style="color:#bbb">药品受照射，在固态时多数是稳定的，少数分解不严重;</p></li>
+                                <li><P style="color:#bbb">分解程度与辐照剂量有关;</p></li>
+                                <li><P style="color:#bbb">剂量与污染的菌种、数量等相关;</p></li>
+                                <li><P style="color:#bbb">水溶液几乎均受到严重破坏;</p></li>
+                                <li><P style="color:#bbb">对一部分药品可以采用先辐照固态灭菌，然后再配成溶液的做法;</p></li>
+                                <li><P style="color:#bbb">大部分经一定剂量射线辐照的中药材,其化学成分在辐照前后无明显变化。</p></li>
                             </ul>
-							</br>
+                          </li>
 
-							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;辐照灭菌对药品质量影响</strong></h5>
+                          <li>对中药材及制剂外观的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照对大多数中药材及中成药的外观没有影响。</P></li>
+                          <li>对药品生理活性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大多数中药及制剂经低于10kGy的射线辐照，其生物活性不受影响。</P></li>
+                          <li>对药品安全性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前用于药品辐射的射线不超过允许的最大剂量，不会使药品产生感生放射性，不存在放射性污染。</P></li>
+                      </ul>
+							        </br>
+
+							        <h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;灭菌效果与辐照剂量</strong></h5>
+                      <ul>
+                          <li><P style="color:#bbb">辐照剂量越大，灭菌效果越好。但辐照剂量过大，对药品中某些有效成分可能会有一定的影响，因此在具体的试验中应确定最佳辐照剂量，并按最佳剂量进行辐照。</P></li>
+                          <li><P style="color:#bbb">美国药典规定高剂量有效灭菌为 2.5 kGy，中剂量为 1 kGy，低剂量为 0.2～0.4 kGy。</P></li>
+                          <li><P style="color:#bbb">我国卫生部的标准规定辐照中药最高耐受剂量为
                             <ul>
-                              <li>对化学成分的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;现今已有许多国家如德国、前苏联、印度等10多个国家对百余种药品进行了研究,对化学药品研究较多,研究结果表明:药品受照射,在固态时多数是稳定的,少数分解不严重,其分解程度与辐照剂量有关,而剂量又与污染的菌种、数量等相关。水溶液几乎均受到严重破坏。对一部分药品可以采用先辐照固态灭菌,然后再配成溶液的做法。大部分经一定剂量射线辐照的中药材,其化学成分在辐照前后无明显变化</P></li>
-                              <li>对中药材及制剂外观的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照对大多数中药材及中成药的外观没有影响</P></li>
-                              <li>对药品生理活性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;大多数中药及制剂经低于10kGy的射线辐照,其生物活性不受影响</P></li>
-                              <li>对药品安全性的影响<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前用于药品辐射的射线不超过允许的最大剂量,不会使药品产生感生放射性,不存在放射性污染</P></li>
+                                <li><P style="color:#bbb">散剂、片剂： 3 kGy</p></li>
+                                <li><P style="color:#bbb">丸剂： 5 kGy</p></li>
+                                <li><P style="color:#bbb">中药原料粉： 6 kGy</p></li>
                             </ul>
-							</br>
+                      </ul>
+ 							        </br>
 
-							<h5><strong>&nbsp;&nbsp;&nbsp;&nbsp;灭菌效果与辐照剂量</strong></h5>
-                            <ul>
-                              <li><P style="color:#bbb">辐照剂量越大,灭菌效果越好。但辐照剂量过大,对药品中某些有效成分可能会有一定的影响,因此在具体的试验中应确定最佳辐照剂量,并按最佳剂量进行辐照</P></li>
-                              <li><P style="color:#bbb">美国药典规定高剂量有效灭菌为 2.5 kGy,中剂量为 1 kGy,低剂量为 0.2～0.4 kGy</P></li>
-                              <li><P style="color:#bbb">我国卫生部的标准规定辐照中药最高耐受剂量为:散剂、片剂 3 kGy；丸剂 5 kGy；中药原料粉 6 kGy</P></li>
-                            </ul>
- 							</br>
+                      <h4><strong>医用器械辐照灭菌主要种类</strong></h4>
+                      <div class="row text-center">
 
-                            <h4><strong>医用器械辐照灭菌主要种类</strong></h4>
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/injector.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
 
-                            <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前辐射灭菌消毒的医用器械种类繁多，主要是以下几大类： </P>                        
-                            <ul>
-                              <li>卫生材料<P style="color:#bbb">卫生材料是以棉织物、无纺布等为材料制作的医用敷料、手术巾、手术衣、急救包和棉球等十几种卫生材料的总称。目前，我国的生产企业约有400多家，43%的企业生产的产品出口。</P></li>
-                              <li>医用乳胶手套<P style="color:#bbb">医用乳胶手套大都采用辐射灭菌法，我国较具规模的生产企业约有30家，年产量约为3亿副。</P></li>
-                              <li>一次性使用的高分子医疗用品<P style="color:#bbb">一次性使用的高分子医疗用品非常多，如注射器、手术器械、手套、采血与输血器具、吸入治疗装置、绷带、胶布、药棉、罩衣、帽子等；手术缝合线、体内插入物（如心脏瓣膜）等。</P></li>
-                              <li>药品包装材料<P style="color:#bbb">药品包装材料主要有药品包装塑料制品、药品包装用复合膜、药品包装用PTP铝箔、疫苗包装瓶和抗生素铝塑组合盖等，国家食品药品监督管理总局已明令眼药水的包装瓶要达到无菌级，所以辐射灭菌在药品包装材料领域有巨大的市场前景。</P></li>
-                            </ul>
-                </br>                           
-                            <div class="alert alert-info" style="background-color:#05173b">
-                            	<h4>参考文献</h4>
-                            	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;李计萍. γ射线辐照灭菌法在中药及其制剂中应用现状和相关问题讨论. [J]中国中药杂志. 2007. 10</p>
-                            	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;朱南康. 王传桢. 医疗保健品和食品的辐射加工. 北京. 中国原子能出版社. 2013. 10</p>
-                            </div>
+                          </div>
 
-                        </div>
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/tap.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                          </figure>
+
+                          </div>
+
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/tampon.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
+
+                          </div>
+                          <div class="col-md-3 col-sm-6 col-xs-12 margin-top30">
+                              <figure>
+                                    <img class="img-responsive" src="assets/images/applications/gloves.jpg" alt="直线电子加速器辐照加工应用大镭核技术成套设备" />
+                              </figure>
+
+                          </div>
+
+                      </div> 
+
+
+                      <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;目前辐射灭菌消毒的医用器械种类繁多，主要是以下几大类： </P>                        
+                      <ul>
+                          <li><P style="color:#bbb"><label style="color:#428bca">卫生材料</label>是以棉织物、无纺布等为材料制作的医用敷料、手术巾、手术衣、急救包和棉球等十几种卫生材料的总称。目前，我国的生产企业约有400多家，43%的企业生产的产品出口。</P></li>
+                          <li><P style="color:#bbb"><label style="color:#428bca">医用乳胶手套</label>大都采用辐射灭菌法，我国较具规模的生产企业约有30家，年产量约为3亿副。</P></li>
+                          <li><P style="color:#bbb"><label style="color:#428bca">一次性使用的高分子医疗用品</label>非常多，如注射器、手术器械、手套、采血与输血器具、吸入治疗装置、绷带、胶布、药棉、罩衣、帽子等；手术缝合线、体内插入物（如心脏瓣膜）等。</P></li>
+                          <li><P style="color:#bbb"><label style="color:#428bca">药品包装材料</label>主要有药品包装塑料制品、药品包装用复合膜、药品包装用PTP铝箔、疫苗包装瓶和抗生素铝塑组合盖等，国家食品药品监督管理总局已明令眼药水的包装瓶要达到无菌级，所以辐射灭菌在药品包装材料领域有巨大的市场前景。</P></li>
+                      </ul>
+                      </br>                           
+                      <div class="alert alert-info" style="background-color:#05173b">
+                          <h4>参考文献</h4>
+                          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;李计萍. γ射线辐照灭菌法在中药及其制剂中应用现状和相关问题讨论. [J]中国中药杂志. 2007. 10</p>
+                          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;朱南康. 王传桢. 医疗保健品和食品的辐射加工. 北京. 中国原子能出版社. 2013. 10</p>
+                      </div>
+
+                  </div>
 
 				    </div>
 						
