babd09a1c6f6f9310e96dbcc56a805d2a5ac1468
renyue li <1064272312@qq.com>
2015-06-06 13:17:43 +0800

    modified product -- modulator page

--- prod-modulator.php
+++ prod-modulator.php
@@ -48,136 +48,125 @@
 				    </aside>
                     
                     <div class="col-md-10"  style="padding:20px">
-                    	<div class="col-md-7">
-                    		<h3><strong>智能高功率脉冲调制器</strong></h3>
-                    		<h4><strong>产品结构特点</strong></h4>
-						    <ul>
-							    <li><p>以氢闸流管为主放电开关的线性脉冲调制器。率先在高功率调制器中采用双闸流管放电技术，以国产器件取代昂贵的进口器件，安全、可靠。</p></li>
-							    <li><p>模块化，高压部件全封闭，结构简单可靠，可按不同功率等级方便组合，功率富裕量大。</p></li>
-							    <li><p>采用变压器与电容双重储能技术，脉冲高压精度高，抗高压打火短路能力强，电源转换效率高，节能。</p></li>
-							    <li><p>独创设计的脉冲形成网络（PFN），脉冲前后沿、平坦度技术指标优异，匹配不同负载调整方便。</p></li>
-							    <li><p>脉冲定时精度高，达ns量级，运行中可任意设定脉冲宽度、重复频率、能谱要求，以适合不同辐射加工工艺和工况要求。</p></li>
-							    <li><p>设备各主要部位都有数据采样长期保存，便于比较分析主要零部件性能徐变趋向，以提前主动维护确保设备正常安全工作。</p></li>
+							<div class="row">
+                    				<h3><strong>智能高功率脉冲调制器</strong></h3></br>
+                    				<h4><strong>产品结构特点</strong></h4>
+                    				<div class="col-md-6">
+						    				<ul>
+							    					<li><p>以氢闸流管为主放电开关的线性脉冲调制器。率先在高功率调制器中采用双闸流管放电技术，以国产器件取代昂贵的进口器件，安全、可靠。</p></li>
+							    					<li><p>模块化，高压部件全封闭，结构简单可靠，可按不同功率等级方便组合，功率富裕量大。</p></li>
+							    					<li><p>采用变压器与电容双重储能技术，脉冲高压精度高，抗高压打火短路能力强，电源转换效率高，节能。</p></li>
+							    					<li><p>独创设计的脉冲形成网络（PFN），脉冲前后沿、平坦度技术指标优异，匹配不同负载调整方便。</p></li>
+							    					<li><p>脉冲定时精度高，达ns量级，运行中可任意设定脉冲宽度、重复频率、能谱要求，以适合不同辐射加工工艺和工况要求。</p></li>
+						    				</ul>
+									</div>
+									<div class="col-md-6">
+								  		  <img alt="直线电子加速器辐照加工应用大镭核技术智能高功率脉冲调制器<" class="img-responsive" src="assets/images/dalei/modulator.jpg">
+							    	</div>
 
-						    </ul>
+									<div class="col-md-12">
+											<ul>
+							    					<li><p>设备各主要部位都有数据采样长期保存，便于比较分析主要零部件性能徐变趋向，以提前主动维护确保设备正常安全工作。</p></li>
+						    				</ul>
+									</div>
+						    		</br>
+							</div>
 
-						    <br/>
+							<div class="row">
+						    		<h4><strong>控制系统</strong></h4>
+						    				<div  class="col-md-6">
 
-						    <h4><strong>控制系统</strong></h4>
-						    
-						   <ul>
-						            <li>扁平化UI人机交互界面，触摸屏操作；</li>
-						            <li>可设置允许远程控制；</li>
-						            <li>嵌入式轻型数据库，实时数据显示，历史数据查询，历史操作记录；</li>
-						            <li>软硬件参数设置，调制器账户加密；</li>
-						            <li>设备故障保护与故障指示。</li>
-						    </ul>
-						  
-                        </div>
-                        <!-- picture -->
-                      
-					    <div class="col-md-5 col-sm-6 col-xs-12"><br/><br/><br/><br/>
-						    <div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "singleItem": true, "navigation": true, "pagination": true, "transitionStyle":"fadeUp"}'>
-							    <div>
-								    <img alt="直线电子加速器辐照加工应用大镭核技术智能高功率脉冲调制器<" class="img-responsive" src="assets/images/dalei/modulator1.jpg">
-							    </div>
-						    </div><br/><br/><br/>	
-						    <div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "singleItem": true, "navigation": true, "pagination": true, "transitionStyle":"fadeUp"}'>
-							    <div>
-								    <img alt="直线电子加速器辐照加工应用大镭核技术调制器控制系统" class="img-responsive" src="assets/images/dalei/modulator2.jpg">
-							    </div>
-						    </div>	
-					    </div>
-					   
-					    
-					    <!-- /picture -->					
-                 
-
-
-                        
-                        <div class="col-md-12"><br/>
-				          <h4><strong>主要技术指标</strong></h4>
+						   							<ul>
+						            						<li>扁平化UI人机交互界面，触摸屏操作；</li></br>
+						            						<li>可设置允许远程控制；</li></br>
+						            						<li>实时电压、电流、温度数据显示，历史数据查询，历史操作记录；</li></br>
+						            						<li>历史数据查询，历史操作记录；</li></br>
+						            						<li>软硬件参数设置，调制器账户加密；</li></br>
+						            						<li>设备故障保护与故障指示。</li>
+						    						</ul>
+                        					</div>
+						  					<div  class="col-md-6">
+								    				<img alt="直线电子加速器辐照加工应用大镭核技术调制器控制系统" class="img-responsive" src="assets/images/dalei/modulator2.jpg">
+							    			</div>
+                        			</div>
+			
+                        			<div class="col-md-12"><br/>
+				          					<h4><strong>主要技术指标</strong></h4>
 				          
-				          <table class="table" style="background-color:rgba(0,0,0,0.1); color:rgba(255,255,255,1);">
-						    <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出脉冲电压（速调管负荷）：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>130KV</span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>输出脉冲电流（速调管负荷）：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>100Amax </span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出高压脉冲功率：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>13MW</span></td>
-                            </tr>
-                             <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出微波脉冲功率： </span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>5.5MW</span></td>
-                            </tr>
-                             <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出最大平均功率：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> 120KW</span></td>
-                            </tr>
-
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲重复频率：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>10～550Hz(连续可调)</span></td>
-                            </tr><tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>频率稳定度：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>≤1×10-5</span></td>
-                            </tr><tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>输出脉冲宽度：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> 8.5us（90%-90%处）可分级设定</span></td>
-                            </tr><tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲前沿：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤0.8us</span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲后沿：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤1.5us</span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span> 脉冲顶部平坦度：</span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>  ≤±0.5%（0.8脉宽范围）</span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲幅度稳定度： </span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤±0.5%（电网电压波动≤±10%，4小时）</span></td>
-                            </tr>
-                            <tr>
-                              <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>电源转换效率： </span></td>
-                              <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≥0.8</span></td>
-                            </tr>
-                          </table> 
+				          							<table class="table" style="background-color:rgba(0,0,0,0.1); color:rgba(255,255,255,1);">
+						    								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出脉冲电压（速调管负荷）：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>130KV</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>输出脉冲电流（速调管负荷）：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>100Amax </span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出高压脉冲功率：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>13MW</span></td>
+                            								</tr>
+                             								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出微波脉冲功率： </span></td>
+                             								 		<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>5.5MW</span></td>
+                            								</tr>
+                             								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>输出最大平均功率：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> 120KW</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲重复频率：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>10～550Hz(连续可调)</span></td>
+                            								</tr>
+															<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>频率稳定度：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>≤1×10-5</span></td>
+                            								</tr><tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>输出脉冲宽度：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> 8.5us（90%-90%处）可分级设定</span></td>
+                            								</tr><tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲前沿：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤0.8us</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲后沿：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤1.5us</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span> 脉冲顶部平坦度：</span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>  ≤±0.5%（0.8脉宽范围）</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>脉冲幅度稳定度： </span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≤±0.5%（电网电压波动≤±10%，4小时）</span></td>
+                            								</tr>
+                            								<tr>
+                              										<td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5);"><span>电源转换效率： </span></td>
+                              										<td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> ≥0.8</span></td>
+                            								</tr>
+                          						</table> 
                          
-                        </div>
+                        				</div>
                        
-				      
-				        <div class="col-md-12">
-					        <hr/>
-					        <h3><strong>产品型号</strong></h3>
-					        <ul>
-						       <li><p>MD-1501输出脉冲功率13MW，平均功率60KW，脉宽1～10us,10~460Hz</p></li>
-						       <li><p>MD-4501 输出脉冲功率13MW，平均功率120KW，脉宽1～17us，10～550Hz</p></li>
-						    </ul>
-					    </div>
+				        				<div class="col-md-12">
+					        					<hr/>
+					        					<h3><strong>产品型号</strong></h3>
+					        					<ul>
+						       							<li><p>MD-1501输出脉冲功率13MW，平均功率60KW，脉宽1～10us,10~460Hz</p></li>
+						       							<li><p>MD-4501 输出脉冲功率13MW，平均功率120KW，脉宽1～17us，10～550Hz</p></li>
+						    					</ul>
+					    				</div>
 
-                    </div>
+                    		</div>
 				      
 				</div>
 			</section>
-			<!--/three picture-->
-
 
 		</div>
 		<!-- /WRAPPER -->
 
    <?php require('footer.html');?>
    <?php require('script.html');?>
-
-
-
-		
+	
 	</body>
 </html>
