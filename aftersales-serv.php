3584cda2ecec8837dabf673cd363e3fb0dcce29a
zhang yaoyao <zyy910203@126.com>
2015-06-11 16:44:26 +0800

    add aftersales content

--- aftersales-serv.php
+++ aftersales-serv.php
@@ -3,132 +3,72 @@
 <!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
 <!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
 
-	<?php require ('head.html');?>
+  <?php require ('head.html');?>
 
   <body class="pattern8"><!-- Available classes for body: boxed , pattern1...pattern10 . Background Image - example add: data-background="assets/images/boxed_background/1.jpg"  -->
 
-		<?php require('nav.html');?>
+    <?php require('nav.html');?>
     
     <!-- WRAPPER -->
-		<div id="wrapper">
+    <div id="wrapper">
 			
-			<!-- REVOLUTION SLIDER -->
-			<div class="fullwidthbanner-container roundedcorners">
-				<div class="fullwidthbanner">
-					<ul>	
-						<li data-transition="fade" data-slotamount="7" data-masterspeed="300" >
-							<img src="assets/images/dummy.png" alt="直线电子加速器辐照加工应用大镭核技术成套设备" data-lazyload="assets/images/dalei/fnal-2.jpg" data-fullwidthcentering="on">
-							<div class="tp-caption medium_text lft"
-							data-x="90" 
-							data-y="180"
-							data-speed="300" 
-							data-start="500" 
-							data-easing="easeOutExpo">大镭核技术
-							</div>
+      <div class="fullwidthbanner-container roundedcorners">
+         <div class="fullwidthbanner">
+           <ul>	
+             <li data-transition="fade" data-slotamount="7" data-masterspeed="300" >
+	       <img src="assets/images/dummy.png" alt="直线电子加速器辐照加工应用大镭核技术成套设备" data-lazyload="assets/images/dalei/aftersale.jpg" data-fullwidthcentering="on">
+	     </li>
+	   </ul>
+	 </div>
+      </div>
 
-							<div class="tp-caption large_text lfb"
-							data-x="90" 
-							data-y="222"
-							data-speed="300" 
-							data-start="800" 
-							data-easing="easeOutExpo">电子直线加速器专业制造<br />
-							</div>
+    <!-- ONE NATION -->
+    <section class="container">
+      <h2><strong>售后服务</strong></h2>
+     
+      <div class="row">
+        <aside class="col-md-2">
 
-							<div class="tp-caption lfb"
-							data-x="90" 
-							data-y="330"
-							data-speed="300" 
-							data-start="1100" 
-							data-easing="easeOutExpo">
-							<a href="brief-intro.php" class="btn btn-primary btn-lg">详细资料</a>
-							</div>
-						</li>
-						<li data-transition="fade" data-slotamount="7" data-masterspeed="300" >
-							<img src="assets/images/dummy.png"  alt="直线电子加速器辐照加工应用大镭核技术成套设备" data-lazyload="assets/images/dalei/fnal-3.jpg" data-fullwidthcentering="on">
-							<div class="tp-caption medium_text lft"
-							data-x="90" 
-							data-y="180"
-							data-speed="300" 
-							data-start="500" 
-							data-easing="easeOutExpo">电子加速器
-							</div>
+						<ul class="nav nav-list">
+							<li><a href="#"><i class="fa fa-chevron-circle-right"></i>联系我们</a></li>
+							<li><a href="#"><i class="fa fa-chevron-circle-right"></i>在线培训</a></li>
+							<li><a href="#"><i class="fa fa-chevron-circle-right"></i>加速器售后条款</a></li>
+							<li><a href="#"><i class="fa fa-chevron-circle-right"></i>调制器售后条款</a></li>
+						</ul>
 
-							<div class="tp-caption large_text lfb"
-							data-x="90" 
-							data-y="222"
-							data-speed="300" 
-							data-start="800" 
-							data-easing="easeOutExpo">智能化、模块化、结构化
-							</div>
+					</aside>
 
-							<div class="tp-caption lfb"
-							data-x="90" 
-							data-y="330"
-							data-speed="300" 
-							data-start="1100" 
-							data-easing="easeOutExpo">
-							<a href="prod-eaccel.php"class="btn btn-primary btn-lg">详细资料</a>
-							</div>
-						</li>
-						<li data-transition="fade" data-slotamount="7" data-masterspeed="300" >
-							<img src="assets/images/dummy.png"  alt="" data-lazyload="assets/images/dalei/fnal-1.jpg" data-fullwidthcentering="on">
-							<div class="tp-caption medium_text lft"
-							data-x="90" 
-							data-y="180"
-							data-speed="300" 
-							data-start="500" 
-							data-easing="easeOutExpo">脉冲调制器
-							</div>
 
-							<div class="tp-caption large_text lfb"
-							data-x="90" 
-							data-y="222"
-							data-speed="300" 
-							data-start="800" 
-							data-easing="easeOutExpo">智能高功率脉冲调制器
-							</div>
+        <div class="col-md-9">
+	
+	<p class="lead ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;为了更好的服务用户的需求，做好指导使用及时售后服务工作，我方本着“用心服务，快人一步”的精神。以“周到的服务、可靠的产品质量”为原则向用户做如下承诺：</p>
+         <h4 style="color:#00BFFF"><strong>一、产品质量承诺</strong></h4>
+<p>
 
-							<div class="tp-caption lfb"
-							data-x="90" 
-							data-y="330"
-							data-speed="300" 
-							data-start="1100" 
-							data-easing="easeOutExpo">
-							<a href="prod-modulator.php" class="btn btn-primary btn-lg">详细资料</a>
-							</div>
-						</li>
-						
-					</ul>
-					<div class="tp-bannertimer"></div>
-				</div>
-			</div>
-			<!-- /REVOLUTION SLIDER -->
+1、产品的制造和检测均符合国家标准。</p>
+<p>
+2、产品在有专业检测人员进行检测，确保产品的各项指标达到贵处的要求。</p>
+<p>
+3、我方所提供的产品在质保期内如果存在质量问题，我方愿派专业人员现场检查处理。</p>
 
-			<!-- ONE NATION -->
-			<section class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<h3 class="page-header nomargin-top">
-							<strong class="styleColor"></strong>
-						</h3>
-						<p></p>
-					</div>
+<h4 style="color:#00BFFF"><strong>二、交货期承诺</strong></h4>
+<p>
 
-					<div class="col-md-6">
+我方确保所提供的产品按照需要方要求时间将货物送到指定地点，若需要方有特殊要求，需提前完工的，我方可与需要方协商，确保及时满足需要方的需求。</p>
 
-						<!--one picture -->
-						<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "singleItem": true, "navigation": true, "pagination": true, "transitionStyle":"fadeUp"}'>
-							<div>
-								<img alt="直线电子加速器辐照加工应用大镭核技术成套设备" class="img-responsive" src="assets/images/dalei/fnal-3.jpg">
-							</div>
-							
-						</div>
-						<!-- /one picture -->
-
-					</div>
-				</div>
-			</section>
-			<!-- /ONE NATION -->
+<h4 style="color:#00BFFF"><strong>三、合理的销售服务管理制度及体系</strong></h4>
+<p>
+1、售前服务。设立专门的销售服务机构，公司将会安排相关专业人士负责到场教正确及时的使用。</p>
+<p>
+2、售中服务。为防止用户使用不当而造成不必要的损失，在设备正常运行验收后，我公司指派专业工程师负责在现场为用户单位提供不受人员限制的维修和使用操作培训。培训内容包括：设备正确操作使用知识，识别初级故障及必要的恢复方法，常见故障排除方法等。确保正确使用该产品，让用户用得安全，放心。</p>
+<p>
+3、售后服务。我公司有一套完善的软件控制系统，可以做到产品的远程监测，定期检测产品的使用情况，确保产品处在良好的使用环境中。并且我公司有专业人员在线协助，提供解决问题的方法。如有需要到现场指导的，公司会在24小时之内安排相关专业人员到指定地点进行及时指导。</p>
+          
+</div>
+</div>
+				
+    </section>
+    <!-- /ONE NATION -->
 
 
 
