52bb3b77989d098978468843b46886237da1a976
lirenyue <1064272312@qq.com>
2015-06-24 16:31:29 +0800

    add the missing pictures

---
+++ solution.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+
+	<?php require ('head.html');?>
+
+	<body class="pattern8"><!-- Available classes for body: boxed , pattern1...pattern10 . Background Image - example add: data-background="assets/images/boxed_background/1.jpg"  -->
+
+		<?php require('nav.html');?>
+
+    <!-- WRAPPER -->
+		<div id="wrapper">
+			<header id="page-title">
+
+                <div class="container">	
+
+		            <h1>电子加速器</h1>
+
+			        <ul class="breadcrumb">
+					   <li><a href="index.php">首页</a></li>
+					   <li><a href="produc-disp.php">产品</a></li>
+					   <li class="active">电子加速器</li>
+				    </ul>
+		        </div>
+            </header>
+			<!--product-->
+            <section class="container">
+
+				<div class="row">
+					<div class="col-md-2" ></div>
+
+                  	<div class="col-md-8" style="padding:20px">
+							<div class="row">
+									<div class="col-md-12">
+											<h3 class="text-center"><strong>电子加速器辐照中心建设导航</strong></h3></br>
+										 	<P>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;电子直线加速器是由微波功率源、电子枪、加速管、真空系统、聚焦系统、冷却与恒温循环系统、束流输运扫描输出系统、结构支撑系统、电气控制系统组成。</P></li>	  
+
+								</div>
+
+								<div class="row">
+										<div class="col-md-12"><h4><strong>设备特点</strong></h4></div>
+										<div class="col-md-6">
+												<ul>
+						    							<li><P>本公司专利技术<label style="color:#FFE100">紧凑型一体化设计</label>，使分散复杂的加速器能在工厂内完成整机组装调试与技术指标测试，整体运至现场安装快速投入运行，从根本上改变了通常现场组装调试真空与出束锻炼周期漫长、质量与技术指标无法保证的弊端。</P></li>
+														<li><P>微波功率源与加速系统一体设计，简洁、可靠，大幅度减少微波传输损耗，提高整机效率。</P></li>
+						    							<li><P>采用精心设计的<label style="color:#FFE100">S波段等a等阻抗盘荷波导加速管</label>，具有较高的加速效率和稳定性，在低功率源情况下仍有较高的加速梯度和较缓的负载特性。</P></li>
+														<li><P>采取<label style="color:#FFE100">直立式加速主体结构</label>，避免了束斑变性和束流偏转损失，有利于束下系统安全高效运行。</P></li>
+												</ul>
+										</div>
+										<div class="col-md-6">
+												<img alt="直线电子加速器辐照加工应用大镭核技术成套设备" class="img-responsive" src="assets/images/dalei/accel-sys.png">
+										</div>	
+										<div class="col-md-12">
+												<ul>
+														<li><P>配备专门设计的<label style="color:#FFE100">二极皮尔斯强流电子枪</label>，球面钡钨钪酸盐阴极，发射电流密度大，工作温度低，蒸散小，使用寿命在1.5万小时以上。</P></li>
+														<li><P>脉冲宽度、重复频率、扫描参数灵活可变，按使用需要<label style="color:#FFE100">智能控制</label>，适应不同加工剂量要求，如灵活适应食品辐照加工和半导体芯片辐照的不同需求。</P></li>
+												</ul>
+										</div>
+										<br/>
+						
+								</div>
+						</div>
+				
+				</div>
+			</section>
+			<!-- /product -->
+
+		</div>
+		<!-- /WRAPPER -->
+
+    <?php require('footer.html');?>
+    <?php require('script.html');?>
+	
+	</body>
+</html>
