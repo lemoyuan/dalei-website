9f9478aabda1617628d9f9688f64a5004bb571b5
lirenyue <1064272312@qq.com>
2015-07-17 16:46:36 +0800

    add center management page

---
+++ center-management.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+
+	<?php require ('head.html');?>
+
+	<body class="pattern8"><!-- Available classes for body: boxed , pattern1...pattern10 . Background Image - example add: data-background="assets/images/boxed_background/1.jpg"  -->
+
+		<?php require('nav.html');?>
+
+		<!-- WRAPPER -->
+		<div id="wrapper">
+			<header id="page-title">
+
+                <div class="container">	
+
+		            <h1>质量管理体系</h1>
+
+			        <ul class="breadcrumb">
+					   <li><a href="produc-disp.php">产品</a></li>
+					   <li><a href="">辐照中心</a></li>
+				    </ul>
+		        </div>
+            </header>
+
+			<!-- ONE NATION -->
+			<section class="container">
+				<div class="row">
+
+                    <div class="col-md-9" style="padding:20px">
+                            <h4><strong>辐照装置验收与加工生效</strong></h4>
+                            <div class="col-md-8"><img src="/assets/images/dalei/certificate.png"></div>
+						    <P>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照装置验收的目的是保证辐照装置安全可靠、正确运行, 确保工作人员和公众的健康和安全, 并有稳定的辐照场以保证辐照产品的质量。</P>
+
+					    	<ul>
+						            <li><P style="color:#bbb">根据国家有关法规, 辐照装置在正式运行前必须经主管部门验收, 并获得“辐照加工许可证”。辐照装置的安全防护设施必须符合国家标准。剂量计必须符合国家计量检测规程的要求, 并根据规程对各检定项目进行测定。</p></li>
+						            <li><P style="color:#bbb">正式运行前, 应根据辐照装置和辐照产品的情况建立辐照模式, 其原则是在保证产品内剂量不均匀度满足要求的条件下, 最大限度地利用辐照空间。</p></li>
+						            <li><P style="color:#bbb">对于静态辐照, 绘制等剂量曲线, 选定产品堆放的位置、方式及翻转倒位的程序。并对不同密度的模拟产品进行剂量分布测定, 验证在上述堆放方式下产品中总平均剂量与剂量不均匀度是否满足加工工艺要求。</p></li>
+					    	</ul>
+
+                            <h4><strong>日常加工管理与组织</strong></h4>
+					    	<ul>
+						            <li><p>安全管理是辐照装置正常运行的首要条件, 也是辐照装置管理者的首要责任，包括：</p>
+											<ul><li><P style="color:#bbb">人员上岗前的培训, 以保证安全操作</p></li>
+													<li><P style="color:#bbb">有效的防护监测设备, 严格执行国家对放射性工作人员个人剂量监测和健康管理的规定</p></li>
+													<li><P style="color:#bbb">保证良好的安全系统和连锁装置, 定期监督安全检查及维修</p></li>
+													<li><P style="color:#bbb">规章制度保证</P></li>
+													<li><P style="color:#bbb">事故管理及处理</P>
+											</ul>
+									</li>
+
+						    	    <li><p>质量管理是确保辐照产品质量的一系列工作体系, 主要包括:</p>
+											<ul><li>辐照工艺管理<P style="color:#bbb">根据辐照目的要求, 制订辐照工艺流程和确定各项工艺参数; 保存辐照过的每批产品的各项工艺参数, 为日后提供原始资料。</P></li>
+													<li>剂量管理<P style="color:#bbb">根据要求的辐照工艺参数、产品包装密度及辐照场中剂量分布确定运行参数, 在产品中布放跟踪剂量计及贴上目视辐照指示片, 对产品中剂量分布及是否漏照进行现场监测。辐照后分析跟踪剂量计, 如有问题, 进行调查。</P></li>
+													<li>运行管理<P style="color:#bbb">产品接收(数量、剂量要求、特殊要求、破损情况)、产品存放(产品批号、存放区号、贮存条件)、辐照模式(静态: 辐照时间、堆放和翻转模式; 动态: 刻度系数、辐照容器、运行状态、停留时间或传送速度) 的确定和实施。</P></li>
+											</ul>
+									</li>
+                                    <div align="center"><img src="/assets/images/dalei/technics.jpg"></div>
+						    	    <li><p>经营管理合理与否直接关系到辐照成本和企业的经济效益。</p>
+											<P style="color:#bbb">影响辐照成本的因子有: 设备的使用率, 能量的利用率, 辐照剂量以及人工、水电等日常消耗。</P>
+											<p>生产管理人员应考虑到以下几点:</p>
+											<ul><li><P style="color:#bbb">考虑辐照产品来源与数量。</P></li>
+													<li><P style="color:#bbb">预测每月、每周、每日工作量, 调整辐照计划。</P></li>
+													<li><P style="color:#bbb">根据设备、人工使用的成本, 调整辐照方式及生产量以降低辐照成本。</P></li>
+													<li><P style="color:#bbb">适时维修以保证生产安全和提高设备使用率。</P></li>
+											</ul>
+									</li>
+
+						    	    <li><p>档案管理大体包括</p>
+											<ul><li><P style="color:#bbb">设备: 辐照装置、辐射源、安全系统</P></li>
+													<li><P style="color:#bbb">剂量系统: 剂量计、比对与校正、剂量分布图、剂量跟踪、剂量分析、环境剂量监测</P></li>
+													<li><P style="color:#bbb">生产运行: 产品收发、运行参数、辐照模式、辐照后管理</P></li>
+													<li><P style="color:#bbb">人员: 岗位任务、培训记录、个人剂量监测、医疗记录</P></li>
+													<li><P style="color:#bbb">财务: 辐照收入、成本核算</P></li>
+													<li><P style="color:#bbb">紧急事件记录。</P></li>
+											</ul>
+											<p>根据产品辐照加工管理的内容, 辐照单位在经理领导下至少设立以下部门: </p>
+											<ul><li><P style="color:#bbb">经营部, 负责对外业务的接洽处理、产品的收发及财务管理</P></li>
+													<li><P style="color:#bbb">质检部, 负责产品工艺的确立及产品质量检查</P></li>
+													<li><P style="color:#bbb">剂量部, 提供辐照场剂量分布图及有关运行参数, 建立日常剂量监督体系</P></li><li><P style="color:#bbb">运行部, 负责日常产品辐照加工及设备维修</P></li>
+											</ul>
+									</li>
+
+						    </ul>
+
+                            <h4><strong>产品辐照过程中的管理</strong></h4>
+						    <ul>
+						        	<li>辐照前处理<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照只能使合格产品提高价值, 不可能把劣质产品变成优质产品。因此辐照前必须对产品的原始质量有严格的要求。</P></li>
+							        <li>包装<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;应符合国家对同类产品的包装要求, 并且应具备安全、无毒和耐辐照的特点。根据不同产品的辐照要求选择包装材料的种类(各种塑料、金属、纤维、玻璃及复合材料) 和功能(防虫、防潮、防止再污染、保护产品等) , 确定对其的特殊要求(真空、透气、气调) 以及外包装箱(材料、形状、大小、密封)。</P></li>
+						            <li>辐照前的贮存和运输<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;对于辐照杀菌的产品，要提供一个防止虫害再侵入和微生物再污染的条件以及产品贮存要求的温、湿度条件和时间长短。</P></li>	
+						            <li>辐照
+											<ul><li><P style="color:#bbb">设备要求(放射源、能量) ,运行操作(根据国际上有关法规) , 辐照装置操作临界控制点, 辐照前后产品的堆放, 辐照指示标记和运行记录
+													<li><P style="color:#bbb">吸收剂量。根据产品的不同情况确定达到辐照目的所需的最小吸收剂量, 根据危害临界控制点(HACCP) 分析确定不影响产品质量的最高剂量阈值
+													<li><P style="color:#bbb">辐照条件。温度、臭氧的排除
+						    				</ul>
+									</li>
+						            <li>辐照后运输和贮存<P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;温度、湿度, 通风, 贮存时间, 防虫和防污染。  </P></li>
+						            <li>标签 <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;标志及辐照说明。</P></li>	
+						            <li>辐照产品质量 <P style="color:#bbb">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;辐照对产品质量是否有影响, 如何避免。为了规范我国食品辐照加工工艺, 国内也颁布了一系列文件。如卫生部发布的18种辐照食品的卫生标准, 从适用范围、引用标准、技术要求、感官指标、理化指标、细菌指标和昆虫指标等方面规定了对辐照食品及辐照工艺的具体要求。我国已对100 多种农副产品和食品进行了辐照加工的研究, 有30 多种辐照食品通过了省、市级技术鉴定。上述大量资料均可作为辐照加工工艺的技术依据。</P></li>	
+						    </ul>
+		       			       			       			       	
+					    </div>
+
+				    <aside class="col-md-3">
+						<div class="widget">
+							<h4><strong>产品</strong></h4>
+						    <ul class="nav nav-list">
+							    <li><a href=""><i class="fa fa-circle-o"></i>建设方案</a></li>
+							    <li><a href="center-management.php"><i class="fa fa-circle-o"></i>质量管理体系</a></li>
+							    <li><a href=""><i class="fa fa-circle-o"></i>辐照工艺管理</a></li>
+						    </ul>
+						</div>
+				    </aside>
+				</div>
+			</section>
+			<!-- /ONE NATION -->
+
+			<section class="container text-center">
+				<h1 class="text-center">
+					<span class="subtitle" style="font-size:24px"></span>
+				</h1>
+			</section>
+
+		</div>
+		<!-- /WRAPPER -->
+
+    <?php require('footer.html');?>
+    <?php require('script.html');?>
+
+	</body>
+</html>
