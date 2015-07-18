c28d4b118af625325e96071c174ab22ed7682fed
lirenyue <1064272312@qq.com>
2015-07-18 14:43:44 +0800

    add construction page

---
+++ center-construt.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if IE 8]>			<html class="ie ie8"> <![endif]-->
+<!--[if IE 9]>			<html class="ie ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->	<html> <!--<![endif]-->
+
+	<?php require ('head.html');?>
+
+	<body class="pattern8"><!-- Available classes for body: boxed , pattern1...pattern10 . Background Image - example add: data-background="assets/images/boxed_background/1.jpg"  -->
+
+		<?php require('nav.html');?>
+
+		<!-- WRAPPER -->
+		<div id="wrapper">
+			<header id="page-title">
+
+                <div class="container">	
+
+		            <h1>建设方案</h1>
+
+			        <ul class="breadcrumb">
+					   <li><a href="produc-disp.php">产品</a></li>
+					   <li><a href="">辐照中心</a></li>
+				    </ul>
+		        </div>
+            </header>
+
+			<!-- ONE NATION -->
+			<section class="container">
+				<div class="row">
+
+                    <div class="col-md-9" style="padding:20px">
+							<p>辐照中心建设主要流程如下：</p>
+                            <div class="row">
+                            <div class="col-md-2"></div>
+                            <div class="col-md-8"><img src="/assets/images/dalei/center.png"></div></div>	
+                            <div class="row">
+                            <div class="col-md-2"></div>	
+                            <div class="col-md-8"><img src="/assets/images/dalei/accel-sys.png"></div></div>	       			       			       	
+					</div>
+
+				    <aside class="col-md-3">
+						<div class="widget">
+							<h4><strong>产品</strong></h4>
+						    <ul class="nav nav-list">
+							    <li><a href="/center-construt.php"><i class="fa fa-circle-o"></i>建设方案</a></li>
+							    <li><a href="/center-management.php"><i class="fa fa-circle-o"></i>质量管理体系</a></li>
+							    <li><a href=""><i class="fa fa-circle-o"></i>辐照工艺管理</a></li>
+						    </ul>
+						</div>
+				    </aside>
+				</div>
+			</section>
+			<!-- /ONE NATION -->
+
+		</div>
+		<!-- /WRAPPER -->
+
+    <?php require('footer.html');?>
+    <?php require('script.html');?>
+
+	</body>
+</html>
