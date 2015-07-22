524e26bef42329b19bdf62630e9efb1471e2f85f
lirenyue <1064272312@qq.com>
2015-07-22 20:18:06 +0800

    changed layout of egun page

--- prod-egun.php
+++ prod-egun.php
@@ -33,36 +33,42 @@
 					<?php require ('prod-aside.html');?>
 
 			        <div class="col-md-9"  style="padding:20px">
+					    <h3><strong>脉冲强流电子枪</strong></h3>
 
-					    <div class="col-md-7">
-					    	<h3><strong>脉冲强流电子枪</strong></h3>
-					    	<h4><strong>产品结构</strong></h4>
-					    	<ul>						
-						      <li> <p>本公司研制的工业辐照加速器用电子枪为二极型的皮尔斯电子枪，由阴极(阴极热子组件)、聚束极和阳极组成。</p></li>
-						    </ul>
-						    <br/>
-						    <h4><strong>产品特点</strong></h4>
+					    <h4><strong>产品结构</strong></h4>
+					    <ul>						
+						    <li> <p>本公司研制的工业辐照加速器用电子枪为二极型的皮尔斯电子枪，由阴极(阴极热子组件)、聚束极和阳极组成。</p></li>
+						 </ul>
+						 <br/>
+
+						<h4><strong>产品特点</strong></h4>
+					    <div class="col-md-6">
 						    <ul>
-						    	<li><p>电子枪的阴极为球面形的钡钨阴极浸渍钪酸盐，具有优良的发射性能，发射电流密度大。在冷态下可短时间暴露大气，阴极激活过程简单。阴极和热子采用组合式结构，热效率高，结构牢固可靠。由于工作温度低，蒸散小，可以有非常长的使用寿命。</p></li>
-						    	<li><p>阳极法兰采用标准CF100法兰与加速管连接。侧面带有一个2升小钛泵。 </p></li>
-						    	<li><p>整个电子枪采用金属——陶瓷封装结构，具有良好的绝缘性、气密性和牢固性，陶瓷表面经特殊处理防止高压爬电打火。 </p></li>
-						    	<li><p>阴极热子组件可方便更换，电子枪寿命终了时只要更换阴极，又可变为一支新枪，大幅度降低使用成本。 </p></li>
-						    	<li><p>经高温烘烤除气并激活测试，可长期保存。 </p></li>   
+								<li>电子枪阴极
+									<ul></br>
+						    			<li><span>电子枪的阴极为球面形的钡钨阴极浸渍钪酸盐，具有优良的发射性能，发射电流密度大。</span></li>
+						    			<li><span>在冷态下可短时间暴露大气，阴极激活过程简单。 </span></li>
+						    			<li><span>阴极和热子采用组合式结构，热效率高，结构牢固可靠。 </span></li>
+						    			<li><span>由于工作温度低，蒸散小，可以有非常长的使用寿命。</span></li>
+									</ul>
+								</li></br>
+						    	<li><p>经高温烘烤除气并激活测试，可长期保存。 </p></li> 
 						    </ul>
 						</div>
 
 						<!--one picture -->
-					    <div class="col-md-5">
-
-						
-						    <div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "singleItem": true, "navigation": true, "pagination": true, "transitionStyle":"fadeUp"}'>
-							    <div><br/><br/><br/><br/><br/><br/>
-								    <img alt="直线电子加速器辐照加工应用大镭核技术电子枪" class="img-responsive" src="assets/images/dalei/egun.jpg">
-							    </div>
-							
-						    </div>
+					    <div class="col-md-6">
+								    <div><img alt="直线电子加速器辐照加工应用大镭核技术电子枪" class="img-responsive" src="assets/images/dalei/egun.jpg"></br></div>					
 				        </div>
 				        <!-- /one picture -->		
+
+					    <div class="col-md-12">
+					    <ul>			
+						    	<li><p>阳极法兰采用标准CF100法兰与加速管连接。侧面带有一个2升小钛泵。 </p></li>
+						    	<li><p>整个电子枪采用金属—陶瓷封装结构，具有良好的绝缘性、气密性和牢固性，陶瓷表面经特殊处理防止高压爬电打火。 </p></li>
+						    	<li><p>阴极热子组件可方便更换，电子枪寿命终了时只要更换阴极，又可变为一支新枪，大幅度降低使用成本。 </p></li>  
+						 </ul>
+						</div>
 
 						<div class="col-md-12"><br/>	
 						    <h4><strong>主要技术指标</strong></h4>
@@ -70,32 +76,24 @@
                                 <tr>
                                     <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>工作阳极电压：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>50～80KV</span></td>
-                                </tr>
-
-                                <tr>
-                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>额定热丝电压：</span></td>
+                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5); "><span>额定热丝电压：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>  12V</span></td>
                                 </tr>
 
                                 <tr>
                                     <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>  额定热丝电流：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>4A</span></td>
-                                </tr>
-
-                                <tr>
-                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>额定脉冲电流：</span></td>
+                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>额定脉冲电流：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span> 600mA</span></td>
                                 </tr>
 
                                 <tr>
                                     <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span> 阳极孔通过率：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>99.999%</span></td>
-                                </tr>
-                                
-                                <tr>
-                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); "><span>束腰离阳极孔距离：</span></td>
+                                    <td width="423"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>束腰离阳极孔距离：</span></td>
                                     <td width="424"  style="border-top:1px dashed rgba(102,102,102,0.5); border-left:1px dashed rgba(102,102,102,0.5);"><span>70～80mm</span></td>
                                 </tr>
+
                             </table>       
 					    </div>
 
