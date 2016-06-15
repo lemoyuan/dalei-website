655fd0c9cbdb18e594c89507d4800bc722c9812e
lirenyue <1064272312@qq.com>
2016-06-15 10:48:31 +0800

    delete baidu map

--- contact-us.php
+++ contact-us.php
@@ -152,7 +152,6 @@
                               	<td class="text-left"><a href="mailto:support@bigradium.com"> support@bigradium.com</a></td>
                             </tr>
 						</table></br>
-						<div style="width:700px;height:250px;border:#ccc solid 1px;font-size:12px" id="map"></div>
 					</div>
 					<!-- /INFO -->
 
@@ -206,63 +205,7 @@
 
 		</div>
 		<!-- /WRAPPER -->
-        
-		<!-- baidu map -->
-		<script type="text/javascript">
-			//创建和初始化地图函数：
-			function initMap(){
-				createMap();//创建地图
-				setMapEvent();//设置地图事件
-				addMapControl();//向地图添加控件
-				addMapOverlay();//向地图添加覆盖物
-			}
-			function createMap(){ 
-				map = new BMap.Map("map"); 
-				map.centerAndZoom(new BMap.Point(122.203801,30.094656),17);
-			}
-			function setMapEvent(){
-				map.enableScrollWheelZoom();
-				map.enableKeyboard();
-				map.enableDragging();
-				map.enableDoubleClickZoom()
-			}
-			function addClickHandler(target,window){
-				target.addEventListener("click",function(){
-					target.openInfoWindow(window);
-				});
-			}
-			function addMapOverlay(){
-				var markers = [{content:"办公地点",title:"浙江大镭核技术应用设备有限公司",imageOffset: {width:0,height:3},position:{lat:30.095328,lng:122.202957}}];
-				for(var index = 0; index < markers.length; index++ ){
-					var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
-					var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
-										imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
-				})});
-				var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
-				var opts = {
-					width: 200,
-					title: markers[index].title,
-					enableMessage: false
-				};
-				var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
-				marker.setLabel(label);
-				addClickHandler(marker,infoWindow);
-				map.addOverlay(marker);
-			};
-		}
-		//向地图添加控件
-		function addMapControl(){
-			var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_LEFT});
-			scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
-			map.addControl(scaleControl);
-			var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
-			map.addControl(navControl);
-			var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:true});
-			map.addControl(overviewControl);
-		}
-		var map;
-		initMap();
-		</script>
+       
 
         <?php require('footer.html');?>
         <?php require('script.html');?>
