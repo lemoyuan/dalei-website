184b934360e7d471000b2a624d23e22dd8444bd4
zhang yaoyao <zyy910203@126.com>
2016-07-07 17:10:25 +0800

    modify page version about enterpr-infor

--- enterpr-infor.php
+++ enterpr-infor.php
@@ -27,15 +27,20 @@
 				</header>
 
 				<section class="container">
+					<!-- Masonry Loader -->
+					<span class="js_loader"><i class="fa fa-asterisk fa-spin"></i>LOADING</span>
 
-					<div class="blog-timeline">
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								6.22<span>2016</span>
-							</div>
+					<ul class="masonry-list">
+
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
-								<!-- add class "controlls-over" if you need the button controlls over the image -->
+								<!-- article title -->
+								<div class="item-title">
+
+								</div>
+
+								<!-- image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
 									<div>
 										<img alt="大镭核技术成套设备" class="img-responsive" src="assets/images/dalei/dyn-160622-2.jpg">
@@ -47,35 +52,41 @@
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;6月22日，浙江省科技厅党组书记、厅长周国辉一行赴舟山调研科技创新工作，在副市长沈仁华等市委领导的陪同下对舟山海洋产业集聚区科技创新工作进行调研，期间实地走访了我公司，考察了我公司加速器设备制造和应用情况。周厅长提出“坚定不移大力推进高新技术产业发展 ”。</p>				
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#36a0da">2016年6月22日 </span>，浙江省科技厅党组书记、厅长周国辉一行赴舟山调研科技创新工作，在副市长沈仁华等市委领导的陪同下对舟山海洋产业集聚区科技创新工作进行调研，期间实地走访了我公司，考察了我公司加速器设备制造和应用情况。周厅长提出“坚定不移大力推进高新技术产业发展 ”。</p>	
+
+
+
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-right" style="background-color:#444444">
-							<div class="item-date">
-								6.12<span>2016</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
-
 								<!-- article title -->
-								<h4 style="color:#36a0da">我公司舟山辐照中心—舟山雷大电子科技有限公司被认定为“2016年舟山市专利示范企业”。</h4>
+								<div class="item-title">
+                                                                            <h4 style="color:#36a0da">我公司舟山辐照中心—舟山雷大电子科技有限公司被认定为“2016年舟山市专利示范企业”。</h4>
+								</div>
+
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;舟山市科技局于2016年6月6日发布了<a href="http://www.zhoushan.gov.cn/web/zhzf/zwdt/bmdt/201606/t20160612_826991.shtml">《关于认定舟山雷大电子科技有限公司等企业为2016年市专利示范企业》</a>的通知，这是我公司一直以来致力于技术创新的成果，我们会再接再厉，不断增强企业竞争力。</p>				
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;舟山市科技局于<span style="color:#36a0da"> 2016年6月6日 </span>发布了<a href="http://www.zhoushan.gov.cn/web/zhzf/zwdt/bmdt/201606/t20160612_826991.shtml">《关于认定舟山雷大电子科技有限公司等企业为2016年市专利示范企业》</a>的通知，这是我公司一直以来致力于技术创新的成果，我们会再接再厉，不断增强企业竞争力。</p>				
+
+
 							</div>
 
-						</article>
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								5.30<span>2016</span>
-							</div>
+						</li>
+
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
+								<!-- article title -->
+								<div class="item-title">
 
-								<!-- add class "controlls-over" if you need the button controlls over the image -->
+								</div>
+
+								<!-- image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
 									<div>
 										<img alt="大镭核技术成套设备" class="img-responsive" src="assets/images/dalei/dyn-160530-2.jpg">
@@ -87,38 +98,41 @@
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5月30日，浙江如山创投董事长王涌一行人到我公司视察，就获得首轮注资后如何加快我公司的战略布局进行讨论。</p>				
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#36a0da">2016年5月30日 </span>，浙江如山创投董事长王涌一行人到我公司视察，就获得首轮注资后如何加快我公司的战略布局进行讨论。</p>				
+
+
+
 							</div>
 
-						</article>
-						<article class="timeline-item pull-right" style="background-color:#444444">
-							<div class="item-date">
-								11.17<span>2015</span>
-							</div>
+						</li>
+
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">“深圳高交会”一展风采</h4>
+								<div class="item-title">
+								  <h4 style="color:#36a0da">“深圳高交会”一展风采 </h4>
+								</div>
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;“泡椒凤爪、卤味豆干、布丁牛奶等食品，若要延长保质期商家会怎么做？不错，就是加防腐剂；纱布、注射器、针管等一次性医用设备，若要杀毒灭菌生产商会怎么做？市面上一般用环氧乙烷。但防腐剂和环氧乙烷是有毒致癌物质，将损害人类健康。
 　　那怎么办？浙江团这次带来的辐照高能电子加速器将改变这一切。浙江大镭核技术应用设备有限公司总工程师陆锐峰告诉记者，通过这套电子加速器设备所产生的β射线可以使得生物体（菌虫类）失去活性，从而延长食品的保质期；也可以改变分子结构，使材料改变性能，比如说通过β射线的辐射的电线可实现不起火不老化。”</p>			
-								<h4><a href="http://zjrb.zjol.com.cn/html/2015-11/17/content_2926877.htm?div=-1" >--《浙江日报》</a>	</h4>		
+								<h4><a href="http://zjrb.zjol.com.cn/html/2015-11/17/content_2926877.htm?div=-1" >--《浙江日报》</a>	</h4>	
+	                                                                <p style="color:#36a0da" class="pull-right">   2015年11月17日 记录 </p>	
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								11.13<span>2015</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">大镭入选市“科技创业企业”助飞计划</h4>
+								<div class="item-title">
+								   <h4 style="color:#36a0da">大镭入选市“科技创业企业”助飞计划 </h4>
+								</div>
 
 								<!-- add class "controlls-over" if you need the button controlls over the image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
@@ -133,14 +147,12 @@
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;“科技创业企业”助飞计划是市委、市政府全面实施创新驱动发展战略的举措之一，旨在进一步推动我市科技型中小微企业快速成长，在全市培育形成一批科技含量高、发展前景好、具有行业竞争优势的“科技创业企业”。入选企业将在三年内获得市科创资金最高1000万元的支持。</p>				
+	                                                                <p style="color:#36a0da" class="pull-right">   2015年11月13日 记录 </p>
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-right" style="background-color:#444444">
-							<div class="item-date">
-								10.30<span>2015</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
@@ -154,22 +166,20 @@
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;根据浙江省科技厅《关于省科技型企业认定有关事项的通知》（浙科发高【2012】286号）的通知，受省科技厅委托，按照《关于浙江省科技型中小企业认定工作实施意见》（浙科发高【2002】201号）有关规定，经研究，现认定浙江大镭核技术应用设备有限公司等53家企业为2015年第二批浙江省科技型企业。2015年10月30日，舟山市科学技术局印发。
-								</p>								
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;根据浙江省科技厅《关于省科技型企业认定有关事项的通知》（浙科发高【2012】286号）的通知，受省科技厅委托，按照《关于浙江省科技型中小企业认定工作实施意见》（浙科发高【2002】201号）有关规定，经研究，现认定浙江大镭核技术应用设备有限公司等53家企业为2015年第二批浙江省科技型企业。<span style="color:#36a0da">2015年10月30日 </span>，舟山市科学技术局印发。
+								</p>
 							</div>
 
-						</article>
-
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								8.20<span>2015</span>
-							</div>
+						</li>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">大镭摘得“第四届创新创业大赛浙江赛区——先进制造专场复赛”探花</h4>
-									
+								<div class="item-title">
+								  <h4 style="color:#36a0da">大镭摘得“第四届创新创业大赛浙江赛区——先进制造专场复赛”探花</h4> 
+								</div>
+
 								<!-- add class "controlls-over" if you need the button controlls over the image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
 									<div>
@@ -183,20 +193,20 @@
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在刚刚谢幕的第四届中国创新创业大赛浙江赛区先进制造专场上，于3D打印、机器人等先进制造的激烈角逐下，大镭创新创业团队凭借工业辐照高能电子加速器项目脱颖而出，最终以88.5分获得了先进制造专场复赛第三名。								</p>								
+	                                                                <p style="color:#36a0da" class="pull-right">   2015年8月20日 记录 </p>
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-right" style="background-color:#444444">
-							<div class="item-date">
-								4.8<span>2015</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">舟山市科技局郭局长莅临大镭考察指导</h4>
-									
+								<div class="item-title">
+								  <h4 style="color:#36a0da">舟山市科技局郭局长莅临大镭考察指导</h4>
+								</div>
+
 								<!-- add class "controlls-over" if you need the button controlls over the image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
 									<div>
@@ -209,21 +219,20 @@
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4月8日，市科技局郭局长一行莅临我公司考察指导，听取了公司创建发展、新产品新技术开发和市场营销等方面的工作汇报。在详细询问公司生产经营过程中遇到的难题后，郭局长表示市科技局将一如既往的给予支持和提供切实的帮助，为企业破解发展难题。
-								</p>								
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#36a0da">2014年4月8日 </span>，市科技局郭局长一行莅临我公司考察指导，听取了公司创建发展、新产品新技术开发和市场营销等方面的工作汇报。在详细询问公司生产经营过程中遇到的难题后，郭局长表示市科技局将一如既往的给予支持和提供切实的帮助，为企业破解发展难题。
+								</p>
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								3.20<span>2015</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">浙江省能源与核技术应用研究院郭院长莅临大镭实地考察</a></h4>
+								<div class="item-title">
+								  <h4 style="color:#36a0da">浙江省能源与核技术应用研究院郭院长莅临大镭实地考察</a></h4>
+								</div>
 
 								<!-- add class "controlls-over" if you need the button controlls over the image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
@@ -239,21 +248,20 @@
 								<p style="color:#bbb;margin:0px">
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;近日，浙江省能源与核技术应用研究院郭院长一行3人莅临我司，实地考察了我司的研发中心和设备运行情况。双方就高能电子直线加速器的制造现状、发展前景、开发合作等事宜进行探讨，郭院长对我司的产品给予高度评价，认为其技术指标先进、智能化程度高，运行稳定。
 								</p>
-
+	                                                                <p style="color:#36a0da" class="pull-right">   2015年3月20日 记录 </p>
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-right" style="background-color:#444444">
-							<div class="item-date">
-								12.30<span>2014</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
 								<!-- article title -->
-								<h4 style="color:#36a0da">大镭实用新型专利获得国家授权</h4>
-									
+								<div class="item-title">
+								  <h4 style="color:#36a0da">大镭实用新型专利获得国家授权</h4>
+								</div>
+
 								<!-- add class "controlls-over" if you need the button controlls over the image -->
 								<div class="owl-carousel controlls-over" data-plugin-options='{"items": 1, "autoHeight": true, "navigation": true, "pagination": false, "transitionStyle":"fadeUp"}'><!-- transitionStyle: fade, backSlide, goDown, fadeUp,  -->
 									<div>
@@ -267,15 +275,13 @@
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
 									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;长久以来，大镭在技术创新和产品研发的道路上坚持不懈，获得的国家专利逐年增加，相关的专利转化成果为公司“推动工业辐照电子直线加速器普及，助力辐照产业壮大”提供了强有力的技术支撑，亦坚定了大镭在技术研发的道路上持之以恒的信念！
-								</p>								
+								</p>
+	                                                                <p style="color:#36a0da" class="pull-right">   2014年12月30日 记录 </p>
 							</div>
 
-						</article>
+						</li>
 
-						<article class="timeline-item pull-left" style="background-color:#444444">
-							<div class="item-date">
-								11.5<span>2014</span>
-							</div>
+						<li class="masonry-item" style="background-color:#444444">
 
 							<div class="item">
 
@@ -291,13 +297,14 @@
 
 								<!-- blog short preview -->
 								<p style="color:#bbb;margin:0px">
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2014年11月5日，我司负责人赴沪参加了由江浙沪核学会和长三角辐射加工联席会主办的第七届长三角辐射加工学术交流会。与会代表们对我司负责人汇报的《高能量β射线设备与海鲜食品高剂量辐照试验》论文表示浓厚的兴趣，积极参与互动，提出自己的观点和看法，现场气氛热烈。
-								</p>								
+									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#36a0da">2014年11月5日</span>，我司负责人赴沪参加了由江浙沪核学会和长三角辐射加工联席会主办的第七届长三角辐射加工学术交流会。与会代表们对我司负责人汇报的《高能量β射线设备与海鲜食品高剂量辐照试验》论文表示浓厚的兴趣，积极参与互动，提出自己的观点和看法，现场气氛热烈。
+								</p>
+
 							</div>
 
-						</article>
-					</div>
+						</li>
 
+					</ul>
 
 					<!-- load more -->
 					<div class="clearfix"></div>
@@ -308,7 +315,10 @@
 				</section>
 
 			</div>
-	    </div>
+		</div>
+		<!-- /WRAPPER -->
+
+				
 
         <?php require('footer.html');?>
         <?php require('script.html');?>
