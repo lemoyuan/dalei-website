d5a8c1fc60ec388f24a90c452adb3434e71c2659
Xie Mengtao <saber940204@163.com>
2016-09-06 09:56:06 +0800

    revision

--- leida-business.php
+++ leida-business.php
@@ -11,7 +11,7 @@
 
 		<!-- WRAPPER -->
 		<div id="wrapper">
-						<header id="page-title">
+		<header id="page-title">
                 <div class="container">	
 
 		                <h1>辐照加工</h1>
@@ -25,179 +25,231 @@
 				<div class="row">
 				   <aside class="col-md-2">
 
-						<ul class="nav nav-list">
+					         <ul class="nav nav-list">
 							<li><a href="leida-business.php"><i class="fa fa-circle-o"></i> 业务范围</a></li>
 							<li><a href="leida.php"><i class="fa fa-circle-o"></i> 舟山辐照中心</a></li>
 						</ul>
 
 				    </aside>
-                    <div class="col-md-10" style="padding:20px">
-		                <h3>辐照加工业务范围</h3>
-                         <div class="divider half-margins"><!-- divider -->
-							    <i class="fa fa-star"></i>
-						 </div>
+                                         <div class="col-md-10" style="padding:20px">
 
-				        <div class="row" style="background-color:rgb(68,68,68);border-radius:6px; padding:10px">
-					        	<p class="lead" style="padding:20px">大蒜、洋葱、土豆、生姜等农产品的辐照<label style="color:#ffe100">抑制发芽</label></p>
-					        <div class="col-md-4">
-							    <div>
-								        <img style="border-radius:60%;" alt="大镭" class="img-responsive" src="assets/images/applications/garlic.jpg">
-							    </div>
+					    <h3><strong>业务范围</strong></h3>
+                        <div class="divider half-margins"><!-- divider -->
+					    	<i class="fa fa-star"></i>
+						</div>
 
-							</div>
-					        <div class="col-md-4">
-							    <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/grainandgreens.jpg">
-							    </div>
+                        <div class="tabs nomargin-top">
 
-							</div>
-					        <div class="col-md-4">
-							   <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/mushroom.jpg">
-							   </div>
+							<!-- tabs -->
+							<ul class="nav nav-tabs">
+								<li class="active">
+									<a href="#tab1" data-toggle="tab">
+										<i class="fa fa-cutlery"></i> 食品篇
+									</a>
+								</li>
+								<li>
+									<a href="#tab2" data-toggle="tab">
+										<i class="fa fa-medkit"></i> 医疗保健篇
+									</a>
+								</li>
+								<li>
+									<a href="#tab3" data-toggle="tab">
+							                     <i class="fa fa-cogs"></i> 工业篇
+									</a>
+								</li>
+                                                                         <li>
+                                                                                   <a href="#tab4"  data-toggle="tab">
+                                                                                     <i class="fa fa-th-large"></i>其他
+                                                                                   </a>
+                                                                         </li>
+							</ul>
+					
+                            <!-- tabs content -->
+                            <div class="tab-content">
+								<div id="tab1" class="tab-pane active">
+                                    <h4><strong>食品辐照</strong></h4>
+                            		<div class="row">
+                           
 
-					       </div>
-					     </div>
-				        <div class="row" style="background-color:rgb(58,58,58);border-radius:6px; padding:10px">
-						       <p class="lead"  style="padding:20px">豆类与谷类、新鲜水果、蔬菜、干果果脯的<label style="color:#ffe100">辐照杀虫、保鲜、延长贮存期</label></strong></h4>
-					        <div class="col-md-4">
-							    <div>
-								        <img style="border-radius:60%;" alt="大镭" class="img-responsive" src="assets/images/applications/garlic.jpg">
-							    </div>
-							</div>
-					        <div class="col-md-4">
-							    <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/grainandgreens.jpg">
-							    </div>
-
-							</div>
-					        <div class="col-md-4">
-							   <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/mushroom.jpg">
-							   </div>
-					       </div>
-					     </div>
-
-				        <div class="row" style="background-color:rgb(68,68,68);border-radius:6px; padding:10px">
-					        	<p class="lead"  style="padding:20px"><label style="color:#ffe100">推迟</label>番茄、蘑菇、冬笋等的<label style="color:#ffe100">成熟</label></p>
-					        <div class="col-md-4">
-							    <div>
-								        <img style="border-radius:60%;" alt="大镭" class="img-responsive" src="assets/images/applications/garlic.jpg">
-							    </div>
-
-							</div>
-					        <div class="col-md-4">
-							    <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/grainandgreens.jpg">
-							    </div>
-
-							</div>
-					        <div class="col-md-4">
-							   <div>
-								       <img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/mushroom.jpg">
-							   </div>
-
-					       </div>
-					     </div>
-
-				        <div class="row" style="background-color:rgb(58,58,58);border-radius:6px; padding:10px">
-
-						   <p class="lead"  style="padding:20px">脱水蔬菜、香辛料及化妆品等<label style="color:#ffe100">深度杀菌，致病菌无所遁形</label></p>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/spice.jpg">
-							</div>
-
-						 </div>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/semi.jpg">
-							</div>
-						 </div>
-
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/yiliao.jpg">
-							</div>
-					      </div>
-						 </div>
-
-				        <div class="row" style="background-color:rgb(68,68,68);border-radius:6px; padding:10px">
-
-						   <p class="lead"  style="padding:20px">医疗器材<label style="color:#ffe100">消毒灭菌，无化学残留，更安全更彻底</label></p>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/spice.jpg">
-							</div>
-
-						 </div>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/semi.jpg">
-							</div>
-						 </div>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/yiliao.jpg">
-							</div>
-
-					      </div>
-						 </div>
-
-				        <div class="row" style="background-color:rgb(58,58,58);border-radius:6px; padding:10px">
-
-						   <p class="lead"  style="padding:20px">轻松达到宠物饲料、儿童玩具的<label style="color:#ffe100">出口卫生标准</label></p>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/petfood.jpg">
-							</div>
-
-						 </div>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/semi.jpg">
-							</div>
-						 </div>
-
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/yiliao.jpg">
-							</div>
-
-					      </div>
-						 </div>
-
-				        <div class="row" style="background-color:rgb(68,68,68);border-radius:6px; padding:10px">
-
-						   <p class="lead"  style="padding:20px">水产品、禽畜肉类(扒鸡、凤爪等)等食品<label style="color:#ffe100">常温杀菌，保持原有风味</label></p>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/roulei.jpg">
-							</div>
-
-						 </div>
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/semi.jpg">
-							</div>
-						 </div>
-
-					     <div class="col-md-4">
-							<div>
-								<img style="border-radius:50%;" alt="大镭" class="img-responsive" src="assets/images/applications/yiliao.jpg">
-							</div>
-
-					      </div>
-						 </div>
+					       	    		<div class="col-md-12">
+                                			<ul>	
+							        <li><span style="color:#bbb"><label style="color:#ffe100">水产品及其制品：</label>鱼、虾、蟹、贝类、鱿鱼丝、鱿鱼块、鱼片、淡菜等</span></li>
+							        <li><span style="color:#bbb"><label style="color:#ffe100">畜禽肉类及其制品：</label>冷冻肉、熟食、泡椒凤爪、卤味、火腿肠、各类罐头、各种肉干等</span></li>
+							        <li><span style="color:#bbb"><label style="color:#ffe100">农产品及其制品：</label>大蒜、洋葱、土豆等的抑制发芽，番茄、蘑菇、冬笋等的延迟成熟</span></li>
+					                          <li><span style="color:#bbb"><label style="color:#ffe100">果蔬类：</label>苹果、菠萝、哈密瓜、梨、桃、香蕉、荔枝、胡萝卜、白菜、芹菜、毛豆、韭菜等的保鲜，延长贮存期</span></li>
+                                                       </ul>
 	
-				</div>
+                             			</div>
+ 
+                                                       <div class="col-md-12">
+
+						<!-- carousel -->
+						<div class="owl-carousel" data-plugin-options='{"items": 4, "singleItem": false, "pagination": false, "autoPlay": 1000, "stopOnHover":true}'>
+							<div>
+                                                                         <img class="img-food" src="assets/images/business/fruit.jpg">
+                                                                </div>
+                                                                <div>
+								<img class="img-food" src="assets/images/business/meats.jpg" >
+							</div>
+							<div>
+								<img class="img-food" src="assets/images/business/mushs.jpg" >
+							</div>
+							<div>
+								<img class="img-food" src="assets/images/business/garlic.jpg" >
+							</div>
+
+						 </div>
+
 					</div>
-			</section>
-			<!-- /ONE NATION -->
+                                                 
+                                                       
 
-		</div>
-		<!-- /WRAPPER -->
+                                        <div class="col-md-1"></div>
 
+						        		<div class="col-md-2"></div>
+						        		
+                            		</div>
+              					       	    		
+                            			<div class="col-md-1"></div>
+                                    </div>
+
+                            	<div id="tab2" class="tab-pane">
+                           			<h4><strong>医疗保健辐照灭菌之优点</strong></h4>
+                            		<div class="row">
+
+					     <div class="col-md-12">
+						        		<ul>
+						        		<li><span style="color:#bbb">辐照灭菌可在室温下进行，适用于热敏感的塑料制品、生物体和药物；</span></li>
+                                                                                            
+							        	<li><span style="color:#bbb">中药、保健品杀菌，最大程度保留有效成份；</span></li>
+                                                                                           
+							        	<li><span style="color:#bbb">医疗器械辐照灭菌，无化学残留，更安全更彻底；</span></li>
+                                                                                           
+							        	<li><span style="color:#bbb">辐照灭菌速度快，操作简单，稳定可靠。</span></li>
+						        		</ul>
+				             </div>
+				     </div>
+                                       
+                                        </br>
+
+                            		<h4><strong>医疗器械辐照</strong></h4>
+                            		<div class="row">
+                            			<div class="col-md-12">
+                                			<ul>
+					            <li><span style="color:#bbb"><label style="color:#ffe100">一次性器具：</label>一次性防护服、一次性注射器、口罩、缝合线、手术刀片，手术手套等</span></li>
+					            <li><span style="color:#bbb"><label style="color:#ffe100">各种容器具：</label> 口罩、外科手术服、手术缝合线、输血器、血浆分离器、血液透析器、血液灌流器、纱布片、纱布垫、医用敷料、止血纱布，培养皿、脱指棉球、牙科器具等</span></li>
+						   <li><span style="color:#bbb"><label style="color:#ffe100">医用耗材：</label>采血管、血液透析器、培养基、心脏支架、血管支架等 </span></li>
+					            <li><span style="color:#bbb"><label style="color:#ffe100">医药品：</label>中药材、胶囊、片剂、药粉等</span></li>
+						</ul>	
+						</div>
+
+                                                       <div class="col-md-12">
+
+						<!-- carousel -->
+						<div class="owl-carousel" data-plugin-options='{"items": 4, "singleItem": false, "pagination": false, "autoPlay": 1000, "stopOnHover":true}'>
+							<div>
+								<img class="img-medical" src="assets/images/business/gloves.jpg">
+							</div>
+							<div>
+								<img class="img-medical" src="assets/images/business/tampon.jpg" >
+							</div>
+							<div>
+								<img class="img-medical" src="assets/images/business/capsule.jpg" >
+							</div>
+							<div>
+								<img class="img-medical" src="assets/images/business/tablet.jpg" >
+							</div>
+						 </div>
+
+					</div>
+                                                       
+                                                         	</div>
+                                                               
+                                                              </div>
+				
+
+                            	<div id="tab3" class="tab-pane">
+                            		<h4><strong>工业化工辐照</strong></h4>
+                            		<div class="row">
+
+					     <div class="col-md-12">
+                                			<ul>	
+							<li><span style="color:#bbb"><label style="color:#ffe100">半导体器件：</label>利用电子束辐照大幅度提高半导体器件的性能，增加产品的附加值；</span></li>							        			
+							<li><span style="color:#bbb"><label style="color:#ffe100">辐照去污：</label>利用电离辐射导致的辐射分解及辐射合成反应处理工业三废；</span></li>
+                                                                <li><span style="color:#bbb"><label style="color:#ffe100">辐照致色：</label>玉器、珍珠、水晶以及各种有色宝石（蓝宝石、托帕石、碧玺、方解石等），提高产品品质，增加附加值；</span></li>
+						         <li><span style="color:#bbb"><label style="color:#ffe100">辐射化工：</label>高分子材料的辐射交联、热收缩材料的辐射加工、涂料的辐射固化、辐射降解丁基橡胶，生产再生橡胶、辐射聚合技术生产水性乳胶等。</span></li>
+                                                       </ul>
+	
+                             			</div>
+                                                       <div class="col-md-12">
+
+						<!-- carousel -->
+						<div class="owl-carousel" data-plugin-options='{"items": 4, "singleItem": false, "pagination": false, "autoPlay": 1000, "stopOnHover":true}'>
+							<div>
+								<img class="img-industry" src="assets/images/business/ide.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/chip.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/jade.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/jewel.jpg" >
+							</div>
+						 </div>
+
+					</div>
+                                                      <div class="col-md-1"></div>
+
+                            		</div>
+
+                                               </div>
+  
+
+                                              <div id="tab4" class="tab-pane">
+                           			<h4><strong>其他产品辐照杀菌</strong></h4>
+                            		<div class="row">
+
+					     <div class="col-md-12">
+                                			<ul>
+					            <li><span style="color:#bbb"><label style="color:#ffe100">香辛料：</label>干辣椒、红辣椒粉、胡椒、肉桂、八角、桂皮等</span></li>
+					            <li><span style="color:#bbb"><label style="color:#ffe100">化妆品：</label> 滑石粉、云母粉、化妆盒、化妆毛刷、中药面膜以及各种膏霜的等</span></li>
+						   <li><span style="color:#bbb"><label style="color:#ffe100">儿童玩具：</label>塑料玩具、毛公仔、咬牙、羽毛制品、玩具印台、卡片、拼装玩具、木樽等</span></li>
+						   <li><span style="color:#bbb"><label style="color:#ffe100">宠物食品：</label>鱼粮、犬粮、猫粮、宠物罐头食品、宠物肉酱、各式肉棒及牛奶骨等</li>
+						</ul>	
+					      </div>
+                                                       <div class="col-md-12">
+
+						<!-- carousel -->
+						<div class="owl-carousel" data-plugin-options='{"items": 4, "singleItem": false, "pagination": false, "autoPlay": 1000, "stopOnHover":true}'>
+							<div>
+								<img class="img-industry" src="assets/images/business/cosmetic.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/toy.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/dog food.jpg" >
+							</div>
+							<div>
+								<img class="img-industry" src="assets/images/business/flavor.jpg" >
+							</div>
+						 </div>
+
+					          </div>
+                                                       
+                                                         	</div>
+                                                               
+                                                              </div>
+
+
+  				          </div>
+                                              </div>
+				    </div>
+						
+				</div>
+				</div>
     <?php require('footer.html');?>
     <?php require('script.html');?>
 
